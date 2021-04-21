1b32beafa64c47775ea89402542c8209550181b6
Tristan Foureur <tristan.foureur@gmail.com>
2021-04-22 01:16:08 +0200

    chore: tests for the searchbar

--- src/app/components/Toolbar/Features/SearchAddress/__tests__/index.test.tsx
+++ src/app/components/Toolbar/Features/SearchAddress/__tests__/index.test.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { SearchAddress } from '..'
+import userEvent from '@testing-library/user-event'
+import { Router } from 'react-router'
+import { createMemoryHistory } from 'history'
+
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => {
@@ -16,7 +20,30 @@ jest.mock('react-i18next', () => ({
 
 describe('<SearchAddress  />', () => {
   it('should match snapshot', () => {
-    const loadingIndicator = render(<SearchAddress />)
-    expect(loadingIndicator.container.firstChild).toMatchSnapshot()
+    const component = render(<SearchAddress />)
+    expect(component.container.firstChild).toMatchSnapshot()
+  })
+
+  it('should display an error on invalid address', async () => {
+    const component = render(<SearchAddress />)
+    const searchBar = await component.findByTestId('searchaddress')
+    userEvent.type(searchBar, 'hello{enter}')
+    const errorElem = screen.getByText('errors.invalidAddress')
+    expect(errorElem).toBeInTheDocument()
+  })
+
+  it('should redirect to the account', async () => {
+    const history = createMemoryHistory()
+    const pushSpy = jest.spyOn(history, 'push') // or 'replace', 'goBack', etc.
+
+    const component = render(
+      <Router history={history}>
+        <SearchAddress />
+      </Router>,
+    )
+
+    const searchBar = await component.findByTestId('searchaddress')
+    userEvent.type(searchBar, 'oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk{enter}')
+    expect(pushSpy).toHaveBeenCalledWith('/account/oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk')
   })
 })
