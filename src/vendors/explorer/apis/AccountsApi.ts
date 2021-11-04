cac52d18004c14704647a623d58f8c729e243331
Matevž Jekovec <matevz@oasisprotocol.org>
2021-11-04 11:38:56 +0100

    hotfix: Return empty list of validators, if oasismonitor.com is down

--- src/vendors/explorer/apis/AccountsApi.ts
+++ src/vendors/explorer/apis/AccountsApi.ts
@@ -557,10 +557,14 @@ export class AccountsApi extends runtime.BaseAPI {
     /**
      */
     async getValidatorsList(requestParameters: GetValidatorsListRequest): Promise<Array<ValidatorRow>> {
+      try {
         const response = await this.getValidatorsListRaw(requestParameters);
         return await response.value();
+      } catch (e) {
+        // TODO: Properly handle this error - should show a warning or so! See #403
+        return [];
+      }
     }
-
 }
 
 /**
