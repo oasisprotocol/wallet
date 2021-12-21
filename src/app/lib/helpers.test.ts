ce36c7a083d9bf7abac3a422368d5d01ca9d3c16
Michal Zielenkiewicz <michziel@gmail.com>
2021-12-21 22:27:19 +0100

    Update src/app/lib/helpers.test.ts
    Co-authored-by: pro-wh <w@oasislabs.com>

--- src/app/lib/helpers.test.ts
+++ src/app/lib/helpers.test.ts
@@ -2,8 +2,8 @@ import { parseNumberToBigInt } from './helpers'
 
 describe('parseNumberToBigInt', () => {
   it('should return a value of type bigint', () => {
-    expect(parseNumberToBigInt(33333.3)).toEqual(33333300000000n)
-    expect(parseNumberToBigInt(2.0121)).toEqual(2012100000n)
-    expect(parseNumberToBigInt(5)).toEqual(5000000000n)
+    expect(parseNumberToBigInt(33333.3)).toEqual(33333_300_000_000n)
+    expect(parseNumberToBigInt(2.0121)).toEqual(2_012_100_000n)
+    expect(parseNumberToBigInt(5)).toEqual(5_000_000_000n)
   })
 })
