f6b5c0669f658d1bca4c0be79c40bfd10da9ccb8
Michal Zielenkiewicz <michziel@gmail.com>
2022-01-11 17:17:03 +0100

    Add basic SendTransaction test

---
+++ src/app/pages/AccountPage/Features/SendTransaction/__tests__/index.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import * as React from 'react'
+import { Provider } from 'react-redux'
+import { configureAppStore } from 'store/configureStore'
+import { SendTransaction } from '..'
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    }
+  },
+}))
+
+const renderComponent = store =>
+  render(
+    <Provider store={store}>
+      <SendTransaction />
+    </Provider>,
+  )
+
+describe('<SendTransaction />', () => {
+  let store: ReturnType<typeof configureAppStore>
+
+  beforeEach(() => {
+    store = configureAppStore()
+  })
+
+  it('should dispatch sendTransaction action on submit', () => {
+    const spy = jest.spyOn(store, 'dispatch')
+    renderComponent(store)
+
+    userEvent.type(screen.getByPlaceholderText('account.sendTransaction.enterAddress'), 'walletAddress')
+    userEvent.type(screen.getByPlaceholderText('0'), '10')
+    userEvent.click(screen.getByRole('button'))
+
+    expect(spy).toHaveBeenCalledWith({
+      payload: {
+        amount: 10,
+        to: 'walletAddress',
+        type: 'transfer',
+      },
+      type: 'transaction/sendTransaction',
+    })
+  })
+})
