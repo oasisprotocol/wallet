27264b1877e02b08642a9490179bd681e05ac9f2
lukaw3d <lukajeran@oasislabs.com>
2022-01-28 19:18:01 +0100

    Test multiline private key

--- src/app/pages/OpenWalletPage/Features/FromPrivateKey/__tests__/index.test.tsx
+++ src/app/pages/OpenWalletPage/Features/FromPrivateKey/__tests__/index.test.tsx
@@ -38,12 +38,29 @@ describe('<FromPrivateKey  />', () => {
     expect(errorElem).toBeInTheDocument()
 
     // A valid phrase should remove the error
-    textbox.setSelectionRange(0, 5)
+    userEvent.clear(textbox)
     userEvent.type(
       textbox,
       'X0jlpvskP1q8E6rHxWRJr7yTvpCuOPEKBGW8gtuVTxfnViTI0s2fBizgMxNzo75Q7w7MxdJXtOLeqDoFUGxxMg==',
     )
     userEvent.click(button)
+    expect(errorElem).not.toBeInTheDocument()
+  })
+
+  it('should allow multiline private keys with envelope', async () => {
+    renderPage(store)
+    const textbox = screen.getByLabelText(/Enter your private key/)
+    const button = screen.getByRole('button', { name: /Open my wallet/ })
+    userEvent.type(
+      textbox,
+      `
+      -----BEGIN ED25519 PRIVATE KEY-----
+      ZqtrV0QtEY/JemfTPbOl9hgk3UxHXfZO42G4sG+XKHThZTM+GvRiqsAgc7magKNN
+      4MEkyO0pi7lJeunILQKiZA==
+      -----END ED25519 PRIVATE KEY-----`,
+    )
+    userEvent.click(button)
+    const errorElem = screen.queryByText(/Invalid private key/)
     expect(errorElem).not.toBeInTheDocument()
   })
 })
