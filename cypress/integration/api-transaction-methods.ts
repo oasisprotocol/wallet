d7ad94a1e1ad0a50024339a13b25623b09ba4d5d
lukaw3d <lukajeran@oasislabs.com>
2022-03-07 23:40:19 +0100

    Ignore API timeouts when checking transaction methods

--- cypress/integration/api-transaction-methods.ts
+++ cypress/integration/api-transaction-methods.ts
@@ -1,27 +1,48 @@
 import * as monitor from 'vendors/monitor'
 import * as oasisscan from 'vendors/oasisscan'
 
+function ignoreTimeoutError() {
+  // Note: This is strongly discouraged. If it stops working, we converted to
+  // jest in https://github.com/oasisprotocol/oasis-wallet-web/pull/736
+  cy.on('fail', error => {
+    if (error.name === 'CypressError' && error.message.includes('timed out waiting')) {
+      return false
+    }
+    throw error
+  })
+}
+
 describe('check all transaction methods are handled', () => {
   it('by parsing latest transactions on oasismonitor', () => {
-    cy.request('https://monitor.oasis.dev/data/transactions?limit=2000')
-      .should((response) => {
-        if (!response.isOkStatusCode) return // Ignore if API is broken
+    ignoreTimeoutError() // Ignore if API is not responding
 
-        response.body.forEach((transactionFromApi) => {
-          const [parsedTransaction] = monitor.parseTransactionsList([transactionFromApi])
-          expect(parsedTransaction.type).to.be.a('string', `parse transaction type "${transactionFromApi.type}" to a string`)
-        })
+    cy.request({
+      url: 'https://monitor.oasis.dev/data/transactions?limit=2000',
+      retryOnNetworkFailure: false,
+      timeout: 5000,
+    }).should(response => {
+      if (!response.isOkStatusCode) return // Ignore if API is broken
+
+      response.body.forEach(transactionFromApi => {
+        const [parsedTransaction] = monitor.parseTransactionsList([transactionFromApi])
+        expect(parsedTransaction.type).to.be.a('string', `parse transaction type "${transactionFromApi.type}" to a string`)
       })
+    })
   })
 
   it('by comparing list of methods from oasisscan', () => {
-    cy.request('https://api.oasisscan.com/mainnet/chain/methods')
-      .should((response) => {
-        if (!response.isOkStatusCode) return // Ignore if API is broken
+    ignoreTimeoutError() // Ignore if API is not responding
 
-        const allApiMethods = response.body.data.list
-        expect(allApiMethods).to.have.length(Object.keys(oasisscan.transactionMethodMap).length)
-        expect(oasisscan.transactionMethodMap).to.have.keys(allApiMethods)
-      })
+    cy.request({
+      url: 'https://api.oasisscan.com/mainnet/chain/methods',
+      retryOnNetworkFailure: false,
+      timeout: 5000,
+    }).should(response => {
+      if (!response.isOkStatusCode) return // Ignore if API is broken
+
+      const allApiMethods = response.body.data.list
+      expect(allApiMethods).to.have.length(Object.keys(oasisscan.transactionMethodMap).length)
+      expect(oasisscan.transactionMethodMap).to.have.keys(allApiMethods)
+    })
   })
 })
