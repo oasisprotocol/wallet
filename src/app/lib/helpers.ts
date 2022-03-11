de967d2368e7b46e3176b7ef9eb0dd4fbf1739c5
lukaw3d <lukajeran@oasislabs.com>
2022-03-11 21:45:58 +0100

    Simplify helpers by reusing @oasisprotocol/client

--- src/app/lib/helpers.ts
+++ src/app/lib/helpers.ts
@@ -1,22 +1,21 @@
 import { bech32 } from 'bech32'
-import { address, quantity, types } from '@oasisprotocol/client'
+import { quantity, staking, types } from '@oasisprotocol/client'
 import { WalletBalance } from 'app/state/wallet/types'
 
 export const uint2hex = (uint: Uint8Array) => Buffer.from(uint).toString('hex')
 export const hex2uint = (hex: string) => new Uint8Array(Buffer.from(hex, 'hex'))
 
 export const shortPublicKey = async (publicKey: Uint8Array) => {
-  return await address.fromData('oasis-core/address: staking', 0, publicKey)
+  return await staking.addressFromPublicKey(publicKey)
 }
 
 export const publicKeyToAddress = async (publicKey: Uint8Array) => {
-  const data = await address.fromData('oasis-core/address: staking', 0, publicKey)
-  return address.toBech32('oasis', data)
+  const data = await staking.addressFromPublicKey(publicKey)
+  return staking.addressToBech32(data)
 }
 
 export const addressToPublicKey = async (addr: string) => {
-  // const data = await address.fromData('oasis-core/address: staking', 0, publicKey)
-  return address.fromBech32('oasis', addr)
+  return staking.addressFromBech32(addr)
 }
 
 export const uint2bigintString = (uint: Uint8Array) => quantity.toBigInt(uint).toString()
