9a03a54d135a004f8f89bd84835a949182a6436a
Michal Zielenkiewicz <michal@oasisprotocol.org>
2022-03-28 17:50:31 +0200

    Remove unused HMR code

--- src/index.tsx
+++ src/index.tsx
@@ -57,13 +57,6 @@ ReactDOM.render(
   MOUNT_NODE,
 )
 
-// Hot reloadable translation json files
-if (module.hot) {
-  module.hot.accept(['./locales/i18n'], () => {
-    // No need to render the App again because i18next works with the hooks
-  })
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
