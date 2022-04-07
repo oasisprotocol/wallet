cc7461e5e19bd0858412c121c70cd4b7867193aa
lukaw3d <lukajeran@oasislabs.com>
2022-04-07 02:16:20 +0200

    Remove react-scripts reference

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,2 @@
-/// <reference types="react-scripts" />
-
 // To solve the issue: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/31245
 /// <reference types="styled-components/cssprop" />
