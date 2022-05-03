cfe691b816c7ee9f9a53c81b68f23d143f28c9e4
lukaw3d <lukajeran@oasislabs.com>
2022-05-03 04:45:05 +0200

    Simplify start script

--- internals/dev-scripts.js
+++
@@ -1,4 +0,0 @@
-const execSync = require('child_process').execSync
-const buildEnv = require('./set-parcel-build-variables.js')
-
-execSync('yarn parcel:dev', { env: buildEnv({ withCSP: false }), stdio: 'inherit' })
