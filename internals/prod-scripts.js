290acd7401c3f3598464ad60ec3c92a00d3351c3
lukaw3d <lukajeran@oasislabs.com>
2022-05-27 23:02:49 +0200

    Print CSP on build

--- internals/prod-scripts.js
+++ internals/prod-scripts.js
@@ -6,5 +6,7 @@ process.env.REACT_APP_BUILD_DATETIME = Date.now().toString()
 process.env.REACT_APP_BUILD_SHA = execSync('git rev-parse HEAD').toString().trim()
 process.env.REACT_APP_META_CSP = getCsp()
 
+console.log(`Content-Security-Policy: ${process.env.REACT_APP_META_CSP}\n`)
+
 execSync('yarn parcel:prod', { stdio: 'inherit' })
 execSync('cp src/robots.txt build/robots.txt', { encoding: 'utf8' })
