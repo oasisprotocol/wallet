22f48f9a38a9afebf30e633a5b4cf03f69cbc2ac
lukaw3d <lukajeran@oasislabs.com>
2022-06-10 20:07:33 +0200

    Fix lint errors

--- internals/jest/jest.base-config.js
+++ internals/jest/jest.base-config.js
@@ -8,7 +8,7 @@ const config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '.*\\.(css|scss|sass)$': '<rootDir>internals/jest/mocks/cssModule.js',
-    '.*\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>internals/jest/mocks/image.js'
+    '.*\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>internals/jest/mocks/image.js',
   },
   modulePaths: ['<rootDir>/src'],
   resetMocks: true,
@@ -16,10 +16,10 @@ const config = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/internals/jest/babelTransform.js'
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/internals/jest/babelTransform.js',
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$'],
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname']
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 }
 
 module.exports = config
--- internals/jest/jest.ext-config.js
+++ internals/jest/jest.ext-config.js
@@ -8,8 +8,8 @@ const config = {
   displayName: 'Extension Wallet',
   testMatch: [
     '<rootDir>/extension/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/extension/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
-  ]
+    '<rootDir>/extension/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
 }
 
 module.exports = config
