90d8fc418a08bfdd016fc07d869927a6ae81996f
lukaw3d <lukajeran@oasislabs.com>
2022-07-05 03:05:21 +0200

    Refactor type-only test for strict translation keys

--- src/locales/__tests__/strict-translation-keys.test.tsx
+++ src/locales/__tests__/strict-translation-keys.test.tsx
@@ -1,32 +1,39 @@
 import { Trans, useTranslation } from 'react-i18next'
 
-/**
- * This type-only test doesn't need to run, it just needs to be compiled. It
- * checks that CustomTypeOptions in react-i18next.d.ts add type-safety to
- * string-based translation keys.
- */
-function TypeOnlyTestTranslationKeys() {
-  const { t } = useTranslation()
+describe('type-only test', () => {
+  describe('translation keys are strictly typed using CustomTypeOptions', () => {
+    it('correct usage', () => {
+      function Component() {
+        const { t } = useTranslation()
+        return (
+          <div>
+            {t('menu.home')}
+            {t('menu.home', 'Default')}
+            <Trans i18nKey="menu.home" t={t} />
+            <Trans i18nKey="menu.home" t={t} defaults="Default" />
+          </div>
+        )
+      }
+      expect(<Component />).toBeDefined()
+    })
 
-  return (
-    <div>
-      {t('menu.home')}
-      {t('menu.home', 'Default')}
-      <Trans i18nKey="menu.home" t={t} />
-      <Trans i18nKey="menu.home" t={t} defaults="Default" />
-
-      {/* @ts-expect-error Expect typescript to detect incorrect key */}
-      {t('menu.does_not_exist')}
-      {/* @ts-expect-error Expect typescript to detect incorrect key */}
-      {t('menu.does_not_exist', 'Default')}
-      {/* @ts-expect-error Expect typescript to detect incorrect key */}
-      <Trans i18nKey="menu.does_not_exist" t={t} />
-      {/* @ts-expect-error Expect typescript to detect incorrect key */}
-      <Trans i18nKey="menu.does_not_exist" t={t} defaults="Default" />
-    </div>
-  )
-}
-
-test('type-only test checking translation keys are strictly typed using CustomTypeOptions', () => {
-  expect(<TypeOnlyTestTranslationKeys />).toBeDefined()
+    it('detect mistakes', () => {
+      function Component() {
+        const { t } = useTranslation()
+        return (
+          <div>
+            {/* @ts-expect-error Expect typescript to detect incorrect key */}
+            {t('menu.does_not_exist')}
+            {/* @ts-expect-error Expect typescript to detect incorrect key */}
+            {t('menu.does_not_exist', 'Default')}
+            {/* @ts-expect-error Expect typescript to detect incorrect key */}
+            <Trans i18nKey="menu.does_not_exist" t={t} />
+            {/* @ts-expect-error Expect typescript to detect incorrect key */}
+            <Trans i18nKey="menu.does_not_exist" t={t} defaults="Default" />
+          </div>
+        )
+      }
+      expect(<Component />).toBeDefined()
+    })
+  })
 })
