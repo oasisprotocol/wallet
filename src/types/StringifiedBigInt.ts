4f481934068bb0aabceb30bbdf2a16c0dd7dbb32
lukaw3d <lukajeran@oasislabs.com>
2022-08-05 17:39:04 +0200

    Workaround to preserve StringifiedBigInt alias name

--- src/types/StringifiedBigInt.ts
+++ src/types/StringifiedBigInt.ts
@@ -4,4 +4,11 @@
  * Redux can't serialize bigint fields, so we stringify them, and mark them.
  * TODO: make this a branded type, that only works with bigint parsing utils.
  */
-export type StringifiedBigInt = string
+export type StringifiedBigInt = string & PreserveAliasName
+
+interface PreserveAliasName extends String {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const testKeepAliasName: { amount: StringifiedBigInt } = {
+  // Hover "amount" to check that the displayed type is StringifiedBigInt (not string)
+  amount: 10n.toString(),
+}
