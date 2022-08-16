a53e922c7b9c092bb6ea043a5ff6307d12460756
lukaw3d <lukajeran@oasislabs.com>
2022-08-17 00:37:32 +0200

    Simplify StringifiedBigInt

--- src/types/StringifiedBigInt.ts
+++ src/types/StringifiedBigInt.ts
@@ -1,14 +1,6 @@
-/**
- * BigInt as a string.
- *
- * Redux can't serialize bigint fields, so we stringify them, and mark them.
- * TODO: make this a branded type, that only works with bigint parsing utils.
- */
+/** Redux can't serialize bigint fields, so we stringify them, and mark them. */
 export type StringifiedBigInt = string & PreserveAliasName
-
 interface PreserveAliasName extends String {}
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const testKeepAliasName: { amount: StringifiedBigInt } = {
-  // Hover "amount" to check that the displayed type is StringifiedBigInt (not string)
-  amount: 10n.toString(),
-}
+
+// Hover to check if inferred variable type is StringifiedBigInt (not string)
+export const testPreserveAliasName = '0' as StringifiedBigInt
