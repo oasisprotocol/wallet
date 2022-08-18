10b08ba656318ec39edd9062716c8ea6292aaa53
lukaw3d <lukajeran@oasislabs.com>
2022-08-18 04:04:17 +0200

    Disable redux-state-sync if not supported, instead of throwing

--- src/app/state/persist/syncTabs.ts
+++ src/app/state/persist/syncTabs.ts
@@ -17,6 +17,9 @@ import { RootState } from 'types'
 /** Syncing tabs is only needed in web app, not in extension. */
 export const needsSyncingTabs = !window.chrome?.runtime?.id
 
+// Simulate with `delete window.BroadcastChannel`
+export const isSyncingTabsSupported = typeof BroadcastChannel === 'function'
+
 const stateSyncConfig: StateSyncConfig = {
   channel: 'oasis_wallet_broadcast_channel',
   broadcastChannelOption: {
@@ -24,13 +27,17 @@ const stateSyncConfig: StateSyncConfig = {
   },
 }
 
-/** Wrap configureStore with redux-state-sync. */
+/** Wrap configureStore with redux-state-sync if native BroadcastChannel is supported. */
 export function configureStoreWithSyncTabs(
   options: ConfigureStoreOptions<RootState>,
 ): EnhancedStore<RootState, AnyAction, Middleware<unknown, RootState, Dispatch<AnyAction>>[]> {
   if (typeof options.middleware !== 'function') throw new Error('Unsupported type of options.middleware')
   if (typeof options.reducer !== 'function') throw new Error('Unsupported type of options.reducer')
   if (!needsSyncingTabs) {
+    return configureStore(options)
+  }
+  if (!isSyncingTabsSupported) {
+    if (!process.env.JEST_WORKER_ID) console.warn('Syncing between tabs is not supported')
     return configureStore(options)
   }
 
