b6a25442e0de07c5630cb84bf7684b4d43e09c90
lukaw3d <lukajeran@oasislabs.com>
2022-08-29 20:07:42 +0200

    Fix and test build timestamp

--- src/app/components/DateFormatter/__tests__/index.test.tsx
+++ src/app/components/DateFormatter/__tests__/index.test.tsx
@@ -1,11 +1,15 @@
 import { render, screen } from '@testing-library/react'
 
 import { intlDateTimeFormat, DateFormatter } from '..'
+import { buildDatetime } from '../../../../../internals/getBuildData'
 
 describe('intlDateTimeFormat', () => {
   it('should format date to human readable format', () => {
     expect(intlDateTimeFormat(1656932251)).toEqual('7/4/2022, 10:57:31 AM')
     expect(intlDateTimeFormat(new Date(Date.UTC(2022, 6, 27, 8, 33, 10)))).toEqual('7/27/2022, 8:33:10 AM')
+  })
+  it('build time should be in the same format as a tested timestamp', () => {
+    expect(Number(buildDatetime)).toBeCloseTo(1656932251, -9) // Less than 16 years difference
   })
 })
 
