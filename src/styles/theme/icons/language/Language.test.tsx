a0813bf943a5b44d82bf79e70dd129e251713b33
lukaw3d <lukajeran@oasislabs.com>
2022-09-16 16:04:59 +0200

    Test language icon for consistency with grommet-icons

---
+++ src/styles/theme/icons/language/Language.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Language } from 'styles/theme/icons/language/Language'
+import { Language as GrommetLanguage } from 'grommet-icons'
+
+test('Custom Language icon should behave like grommet-icons', () => {
+  const customIcon = render(<Language />)
+  const grommetIcon = render(<GrommetLanguage />)
+  customIcon.container.querySelector('path')!.remove()
+  grommetIcon.container.querySelector('path')!.remove()
+  expect(customIcon.container.innerHTML).toEqual(grommetIcon.container.innerHTML)
+})
