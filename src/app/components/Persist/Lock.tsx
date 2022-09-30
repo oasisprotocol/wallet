b05f62963f5a9adea706a1dfe0a619d4bcffa31e
lukaw3d <lukajeran@oasislabs.com>
2022-09-30 04:05:04 +0200

    Navigate home before erasing profile so there's no infinite wallet loading

--- src/app/components/Persist/Lock.tsx
+++ src/app/components/Persist/Lock.tsx
@@ -3,9 +3,11 @@ import { selectHasPersistedProfiles } from 'app/state/persist/selectors'
 import { Box, Button } from 'grommet'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export function Lock() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const hasPersistedProfiles = useSelector(selectHasPersistedProfiles)
 
   if (!hasPersistedProfiles) {
@@ -16,7 +18,13 @@ export function Lock() {
     <Box direction="row" gap="small">
       <Button label="Lock profile" onClick={() => dispatch(persistActions.lockAsync())}></Button>
 
-      <Button label="Erase profile" onClick={() => dispatch(persistActions.eraseAsync())}></Button>
+      <Button
+        label="Erase profile"
+        onClick={() => {
+          navigate('/')
+          dispatch(persistActions.eraseAsync())
+        }}
+      ></Button>
     </Box>
   )
 }
