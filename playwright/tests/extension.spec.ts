71bed57439fad6e45404b7ab7301ffaaa82d63a9
lukaw3d <lukajeran@oasislabs.com>
2022-10-28 01:59:51 +0200

    Playwright E2E test extension homepage

--- playwright/tests/extension.spec.ts
+++ playwright/tests/extension.spec.ts
@@ -1,13 +1,17 @@
 import { test as base, expect, BrowserContext, chromium } from '@playwright/test'
 import path from 'path'
 
+import extensionManifest from '../../build-dev/manifest.json'
+const pathToExtension = path.join(__dirname, '../../build-dev')
+const popupFile = extensionManifest.browser_action.default_popup
+
 // From https://playwright.dev/docs/chrome-extensions
 export const test = base.extend<{
   context: BrowserContext
   extensionId: string
 }>({
+  // eslint-disable-next-line no-empty-pattern
   context: async ({}, use) => {
-    const pathToExtension = path.join(__dirname, 'my-extension')
     const context = await chromium.launchPersistentContext('', {
       headless: false,
       args: [`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
@@ -16,23 +20,23 @@ export const test = base.extend<{
     await context.close()
   },
   extensionId: async ({ context }, use) => {
-    /*
     // for manifest v2:
     let [background] = context.backgroundPages()
-    if (!background)
-      background = await context.waitForEvent("backgroundpage")
-    */
+    if (!background) background = await context.waitForEvent('backgroundpage')
 
     // for manifest v3:
-    let [background] = context.serviceWorkers()
-    if (!background) background = await context.waitForEvent('serviceworker')
+    // let [background] = context.serviceWorkers()
+    // if (!background) background = await context.waitForEvent('serviceworker')
 
     const extensionId = background.url().split('/')[2]
     await use(extensionId)
   },
 })
 
-test('popup page', async ({ page, extensionId }) => {
-  await page.goto(`chrome-extension://${extensionId}/popup.html`)
-  await expect(page.locator('body')).toHaveText('my-extension popup')
+test.describe('The extension homepage should load', () => {
+  test('should have options to open the wallet', async ({ page, extensionId }) => {
+    await page.goto(`chrome-extension://${extensionId}/${popupFile}`)
+    await expect(page.getByRole('link', { name: /Open wallet/i })).toBeVisible()
+    await expect(page.getByRole('link', { name: /Create wallet/i })).toBeVisible()
+  })
 })
