d7d846bcdc5c85ee7bef66c2975fac7a8fe70d0d
lukaw3d <lukajeran@oasislabs.com>
2022-11-02 02:36:40 +0100

    Refactor playwright tests

--- playwright/tests/syncTabs.spec.ts
+++ playwright/tests/syncTabs.spec.ts
@@ -1,102 +1,86 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
 import { mockApi } from '../utils/mockApi'
 import { warnSlowApi } from '../utils/warnSlowApi'
 import { expectNoFatal } from '../utils/expectNoFatal'
-
-const privateKey = 'X0jlpvskP1q8E6rHxWRJr7yTvpCuOPEKBGW8gtuVTxfnViTI0s2fBizgMxNzo75Q7w7MxdJXtOLeqDoFUGxxMg=='
-const password = 'abcd1234&'
+import { privateKey, password, privateKeyAddress } from '../utils/test-inputs'
+import { addPersistedStorage, clearPersistedStorage } from '../utils/storage'
 
 test.beforeEach(async ({ context, page }) => {
   await warnSlowApi(context)
   await mockApi(context, 0)
+  await clearPersistedStorage(page)
 })
 
 test.afterEach(async ({ context }, testInfo) => {
   await expectNoFatal(context, testInfo)
 })
 
-test('no infinite loading in second tab after closing wallet', async ({ page, context }) => {
-  await page.goto('/open-wallet/private-key')
-  await page.getByPlaceholder('Enter your private key here').fill(privateKey)
-  await page.getByPlaceholder('Enter your private key here').press('Enter')
-  await expect(page.getByText('Loading account')).toBeVisible()
-  await expect(page.getByText('Loading account')).toBeHidden()
-  await expect(page.getByTestId('account-selector')).toBeVisible()
-  await expect(page).toHaveURL('http://localhost:3000/account/oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk')
+test.describe('syncTabs', () => {
+  test.describe('lock second tab after locking wallet', () => {
+    test('unpersisted', async ({ page, context }) => {
+      await page.goto('/open-wallet/private-key')
+      await page.getByPlaceholder('Enter your private key here').fill(privateKey)
+      await page.keyboard.press('Enter')
+      await expect(page.getByText('Loading account')).toBeVisible()
+      await expect(page.getByText('Loading account')).toBeHidden()
+      await expect(page.getByTestId('account-selector')).toBeVisible()
+      await expect(page).toHaveURL(new RegExp(`/account/${privateKeyAddress}`))
 
-  // Second tab should sync the opened wallet
-  const tab2 = await context.newPage()
-  await tab2.goto('/')
-  await expect(tab2.getByTestId('account-selector')).toBeVisible()
-  await tab2.getByRole('link', { name: 'Wallet' }).click()
-  await expect(tab2).toHaveURL('http://localhost:3000/account/oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk')
+      const tab2 = await context.newPage()
+      await testLockingIsSynced(page, tab2)
+    })
 
-  // Second tab should not get stuck on loading after first tab closes wallet
-  await page.getByRole('button', { name: 'Close wallet' }).click()
-  await expect(page.getByText('Loading account')).toBeHidden()
-  await expect(page.getByTestId('account-selector')).toBeHidden()
-  await expect(tab2.getByText('Loading account')).toBeHidden()
-  await expect(tab2.getByTestId('account-selector')).toBeHidden()
-})
+    test('persisted', async ({ page, context }) => {
+      await page.goto('/open-wallet/private-key')
+      await page.getByPlaceholder('Enter your private key here').fill(privateKey)
+      await page.getByText('Store private keys locally, protected by a password').check()
+      await page.getByPlaceholder('Enter your password here').fill(password)
+      await page.getByPlaceholder('Re-enter your password').fill(password)
+      await page.keyboard.press('Enter')
+      await expect(page.getByText('Loading account')).toBeVisible()
+      await expect(page.getByText('Loading account')).toBeHidden()
+      await expect(page.getByTestId('account-selector')).toBeVisible()
+      await expect(page).toHaveURL(new RegExp(`/account/${privateKeyAddress}`))
 
-test('lock second tab after locking wallet', async ({ page, context }) => {
-  await page.goto('/')
-  await page.evaluate(() => window.localStorage.clear())
+      const tab2 = await context.newPage()
+      await testLockingIsSynced(page, tab2)
+      await expect(page.getByRole('button', { name: 'Unlock' })).toBeVisible()
+      await expect(tab2.getByRole('button', { name: 'Unlock' })).toBeVisible()
+    })
 
-  await page.goto('/open-wallet/private-key')
-  await page.getByPlaceholder('Enter your private key here').fill(privateKey)
-  await page.getByText('Store private keys locally, protected by a password').check()
-  await page.getByPlaceholder('Enter your password here').fill(password)
-  await page.getByPlaceholder('Re-enter your password').fill(password)
-  await page.getByPlaceholder('Enter your private key here').press('Enter')
-  await expect(page.getByText('Loading account')).toBeVisible()
-  await expect(page.getByText('Loading account')).toBeHidden()
-  await expect(page.getByTestId('account-selector')).toBeVisible()
-  await expect(page).toHaveURL('http://localhost:3000/account/oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk')
+    test('incognito', async ({ page, context }) => {
+      await addPersistedStorage(page)
+      await page.goto('/')
+      await page.getByRole('button', { name: 'Continue without the profile' }).click()
+      await expect(page.getByTestId('account-selector')).toBeHidden()
 
-  // Second tab should sync the unlocked wallet
-  const tab2 = await context.newPage()
-  await tab2.goto('/')
-  await expect(tab2.getByTestId('account-selector')).toBeVisible()
-  await tab2.getByRole('link', { name: 'Wallet' }).click()
-  await expect(tab2).toHaveURL('http://localhost:3000/account/oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk')
+      const tab2 = await context.newPage()
+      await tab2.goto('/')
+      await expect(tab2.getByRole('button', { name: 'Unlock' })).toBeHidden()
+      await tab2.goto('/open-wallet/private-key')
+      await tab2.getByPlaceholder('Enter your private key here').fill(privateKey)
+      await tab2.keyboard.press('Enter')
+      await expect(tab2.getByTestId('account-selector')).toBeVisible()
+      await expect(page.getByTestId('account-selector')).toBeVisible()
 
-  // Second tab should get locked after first tab locks wallet
-  await page.getByRole('button', { name: 'Lock profile' }).click()
-  await expect(page.getByText('Loading account')).toBeHidden()
-  await expect(page.getByRole('button', { name: 'Unlock' })).toBeVisible()
-  await expect(tab2.getByText('Loading account')).toBeHidden()
-  await expect(tab2.getByRole('button', { name: 'Unlock' })).toBeVisible()
-})
+      await testLockingIsSynced(page, tab2)
+      await expect(page.getByRole('button', { name: 'Unlock' })).toBeVisible()
+      await expect(tab2.getByRole('button', { name: 'Unlock' })).toBeVisible()
+    })
 
-test('lock second tab after closing incognito wallet', async ({ page, context }) => {
-  await page.goto('/')
-  await page.evaluate(() => window.localStorage.clear())
+    async function testLockingIsSynced(page: Page, tab2: Page) {
+      // Second tab should sync the opened wallet
+      await tab2.goto('/')
+      await expect(tab2.getByTestId('account-selector')).toBeVisible()
+      await tab2.getByRole('link', { name: 'Wallet' }).click()
+      await expect(tab2).toHaveURL(new RegExp(`/account/${privateKeyAddress}`))
 
-  await page.goto('/open-wallet/private-key')
-  await page.getByPlaceholder('Enter your private key here').fill(privateKey)
-  await page.getByText('Store private keys locally, protected by a password').check()
-  await page.getByPlaceholder('Enter your password here').fill(password)
-  await page.getByPlaceholder('Re-enter your password').fill(password)
-  await page.getByPlaceholder('Enter your private key here').press('Enter')
-  await page.getByRole('button', { name: 'Lock profile' }).click()
-  await page.getByRole('button', { name: 'Continue without the profile' }).click()
-  await expect(page.getByTestId('account-selector')).toBeHidden()
-
-  // Second tab should sync the incognito wallet
-  const tab2 = await context.newPage()
-  await tab2.goto('/')
-  await expect(tab2.getByRole('button', { name: 'Unlock' })).toBeHidden()
-  await tab2.goto('/open-wallet/private-key')
-  await tab2.getByPlaceholder('Enter your private key here').fill(privateKey)
-  await tab2.getByPlaceholder('Enter your private key here').press('Enter')
-  await expect(tab2.getByTestId('account-selector')).toBeVisible()
-  await expect(page.getByTestId('account-selector')).toBeVisible()
-
-  // Second tab should get locked after first tab closes wallet
-  await page.getByRole('button', { name: 'Close wallet' }).click()
-  await expect(page.getByText('Loading account')).toBeHidden()
-  await expect(page.getByRole('button', { name: 'Unlock' })).toBeVisible()
-  await expect(tab2.getByText('Loading account')).toBeHidden()
-  await expect(tab2.getByRole('button', { name: 'Unlock' })).toBeVisible()
+      // Second tab should not get stuck on loading after first tab closes wallet
+      await page.getByRole('button', { name: /(Close wallet)|(Lock profile)/ }).click()
+      await expect(page.getByText('Loading account')).toBeHidden()
+      await expect(page.getByTestId('account-selector')).toBeHidden()
+      await expect(tab2.getByText('Loading account')).toBeHidden()
+      await expect(tab2.getByTestId('account-selector')).toBeHidden()
+    }
+  })
 })
---
+++ playwright/utils/test-inputs.ts
@@ -0,0 +1,14 @@
+export const privateKeyAddress = 'oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk'
+export const privateKey =
+  'X0jlpvskP1q8E6rHxWRJr7yTvpCuOPEKBGW8gtuVTxfnViTI0s2fBizgMxNzo75Q7w7MxdJXtOLeqDoFUGxxMg=='
+
+export const password = 'abcd1234&'
+
+export const privateKeyPersistedState = JSON.stringify({
+  secretbox: {
+    $Uint8Array$:
+      'pK6b5ybX69rk3dbOY8s1BW4d+uruhCWPqih4jIlxGzb0pYSc5Ccdu0diU8nH89mIc0DYsPtb3BJe/P5iHQz5MhH1RFlmWKxwi2Updkof5HnW5lKuukSTMTQhZrv5ESLWIG0Yihn8g82R9yccoMRxkV32OOMq3ZPHa4o/D00flUvD5ifIhleBkzMkIpuaYbsKQW/yL3D7Hc8WK0zrxk22w7jbu9qvS5Qz5+34L9ZefEQ/I5Ccx2u3ApgHCILFwmKkGVsIAJuZOtJDUn3ximKE/ARZv6hpu7spfJxOMzYkRVpctU2tgaX9dfLhr/KaeoC15R7yAyrVLZNZ9dwQu4GuFYpgyBtDCqEYjqK83AulFqMtl6/Xt3YnhQw/SUyyfeDQRJVXEIsteNMgveq6/9WUf1PUfvraJu+F+gepovrx1QUdmp/ZDieYvVlf+3Weq4WrRG9Qf/x5XInNRbCkQn33ytH6PLXcXyL5zRsFI7IQbTiGc6cvGiP9UwqL6YsKuZ3qIAUNljU/flrlMuPcQ4ZdlAZCSvSlNR+hp+xchUJIyiTwKsdJjGGaCpHi0GMG4nRMp8NPCCf4nK/XBBtN0ljKmgQD7NsHv9Mc/Xz2XgZWSWTCf92I/kig6SdJopGFOZQQLf0FIxBGSBh4DBf2AMnOe5+SP8CrrRJsV5+4V9H7Abv22RgDMMlNps1zVBOBnF+Cq3p4jmYcLlSL0nGXjw9GjZxAhrqix+VnNZHS4CEDsI8I4umvzPC7vNkimcMf1wn0hLtCo7FyEqb1l6EQfpkO3qZJKlg7gye413MP0qwS1V9JmRra/Uiw6PKuRwMUNaJBrH9kWO/FETQ6ECd64+SQ+A5cp0+5tu8nueVbFkaS7slQC2boF5aYvBl9Gohr/2SBG5BM3CuXoaySgWx8mpZYw17WAM8sv6fq5P6HBC2wjG5HnAaU0qfn2AXdNzs6VS0wgVMzINAvBVcgFCBdjFuxDQuwboNIMbS9/S/H13EN28i+VFtAdTfd',
+  },
+  nonce: { $Uint8Array$: 'RzVNG7x7+CA+om8DzkwOb4HT9rRkCAIe' },
+  salt: { $Uint8Array$: 'hPXgdeNf3rbz6JqaUbduwv0x5ckKCYSVEI9RekpaYDo=' },
+})
