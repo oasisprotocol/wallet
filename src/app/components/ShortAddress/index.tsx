9b47288934c85b243d761b13b334be7af990e318
lukaw3d <lukajeran@oasislabs.com>
2022-11-08 23:52:27 +0100

    short address with css ellipsis

--- src/app/components/ShortAddress/index.tsx
+++ src/app/components/ShortAddress/index.tsx
@@ -5,15 +5,39 @@
  */
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 
 interface Props {
   address: string
 }
 
+const MiddleEllipsisSpan = styled.span`
+  display: flex;
+  white-space: nowrap;
+
+  > :first-child {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    min-width: 5ch;
+    max-width: 9.4ch;
+    margin-right: -3px;
+  }
+
+  > :last-child {
+    flex-shrink: 0;
+  }
+`
+
 export function ShortAddress(props: Props) {
   const { t } = useTranslation()
 
   const a = props.address
-  const short = props.address ? `${a.slice(0, 10)}...${a.slice(-8)}` : t('common.unavailable', 'Unavailable')
-  return <>{short}</>
+  if (!a) return <span>{t('common.unavailable', 'Unavailable')}</span>
+
+  return (
+    <MiddleEllipsisSpan>
+      <span>{a.slice(0, -8)}</span>
+      <span>{a.slice(-8)}</span>
+    </MiddleEllipsisSpan>
+  )
 }
