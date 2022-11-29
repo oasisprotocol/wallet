a1896f4a1a2ea3c466d76b1a46dbd1cca5f5b63e
lukaw3d <lukajeran@oasislabs.com>
2022-11-29 22:17:47 +0100

    Don't use polling in expectShell to make tests more reliable
    In assertions:
    expectPasswordInUserData(userDataDir).toContain('exit code 1')
    expectPasswordInUserData(userDataDir).toContain('sessionstore.jsonlz4')
    
    Polling would make not-found assertions unreliable if the browser hasn't written
    user data yet, before the first poll attempt.

--- playwright/utils/expectShell.ts
+++ playwright/utils/expectShell.ts
@@ -2,11 +2,11 @@ import { execSync } from 'child_process'
 import { expect } from '@playwright/test'
 
 export function expectShell(cmd: string) {
-  return expect.poll(() => {
-    try {
-      return execSync(cmd, { encoding: 'utf-8' })
-    } catch (e) {
-      return `exit code ${e.status}: ${e.message}`
-    }
-  })
+  let output: string
+  try {
+    output = execSync(cmd, { encoding: 'utf-8' })
+  } catch (e) {
+    output = `exit code ${e.status}: ${e.message}`
+  }
+  return expect(output)
 }
