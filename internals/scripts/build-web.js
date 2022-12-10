5b86086b067ba134fddbadcc1976fd22b08666d9
lukaw3d <lukajeran@oasislabs.com>
2022-12-10 03:41:59 +0100

    Move build scripts into internals/scripts

--- internals/scripts/build-web.js
+++ internals/scripts/build-web.js
@@ -1,7 +1,7 @@
 // @ts-check
 const execSync = require('child_process').execSync
-const { getCsp } = require('./getCsp.js')
-const { buildDatetime, buildSha, buildVersion } = require('./getBuildData')
+const { getCsp } = require('../getCsp.js')
+const { buildDatetime, buildSha, buildVersion } = require('../getBuildData')
 
 process.env.REACT_APP_BUILD_DATETIME = buildDatetime
 process.env.REACT_APP_BUILD_SHA = buildSha
