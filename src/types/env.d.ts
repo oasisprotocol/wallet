d997a7c9f37ff2b8a5b14e21296d467754a4e141
lukaw3d <lukajeran@oasislabs.com>
2023-01-06 05:40:12 +0100

    fixup! env vars

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,8 +1,14 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    REACT_APP_BACKEND: 'oasismonitor' | 'oasisscan'
-    REACT_APP_LOCALNET: '1' | undefined
-    REACT_APP_E2E_TEST: '1' | undefined
-    NODE_ENV: 'development' | 'production' | 'test'
-  }
+/// <reference types="vite/client" />
+
+// https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
+
+interface ImportMetaEnv {
+  REACT_APP_BACKEND: 'oasismonitor' | 'oasisscan'
+  REACT_APP_LOCALNET: '1' | undefined
+  REACT_APP_E2E_TEST: '1' | undefined
+  NODE_ENV: 'development' | 'production' | 'test'
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
