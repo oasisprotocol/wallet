805ff5270abe2a04f892a761175d52bdd5908de6
lukaw3d <lukajeran@oasislabs.com>
2023-01-18 04:33:55 +0100

    Migrate from @testing-library/react-hooks to @testing-library/react

--- src/app/useRouteRedirects.test.ts
+++ src/app/useRouteRedirects.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react'
 import { useSelector } from 'react-redux'
 import { selectAddress } from 'app/state/wallet/selectors'
 import { useRouteRedirects } from './useRouteRedirects'
