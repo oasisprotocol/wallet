9096a156ed13420f88b3ef4cc3f9cef79670a272
lukaw3d <lukajeran@oasislabs.com>
2023-03-02 17:47:44 +0100

    Fix build if there are no tags (fixes cloudflare shallow clone)

--- internals/getBuildData.js
+++ internals/getBuildData.js
@@ -1,6 +1,9 @@
 // @ts-check
 const buildSha = require('child_process').execSync('git rev-parse HEAD').toString().trim()
-const buildVersion = require('child_process').execSync('git describe --tags --abbrev=0').toString().trim()
+const buildVersion = require('child_process')
+  .execSync('git describe --tags --abbrev=0 --always')
+  .toString()
+  .trim()
 const buildDatetime = Date.now().toString()
 
 module.exports = { buildSha, buildVersion, buildDatetime }
