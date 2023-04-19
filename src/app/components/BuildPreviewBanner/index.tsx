e613f68a3c3e4fde4fdfc3dadd887b04c55a991b
lukaw3d <lukajeran@oasislabs.com>
2023-04-19 19:55:51 +0200

    Show a new domain banner

--- src/app/components/BuildPreviewBanner/index.tsx
+++ src/app/components/BuildPreviewBanner/index.tsx
@@ -1,14 +1,40 @@
-import { useTranslation } from 'react-i18next'
-import { Alert } from 'grommet-icons'
+import { Trans, useTranslation } from 'react-i18next'
+import { Alert, Info } from 'grommet-icons'
 import { Box, Text } from 'grommet'
 import { AlertBox } from 'app/components/AlertBox'
 import { mobileHeaderZIndex } from '../Sidebar'
+import { AnchorLink } from '../AnchorLink'
 
 export const BuildPreviewBanner = () => {
   const { t } = useTranslation()
 
   if (!process.env.REACT_APP_BUILD_PREVIEW) {
-    return null
+    return (
+      <Box
+        background={{ color: 'white' }}
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: mobileHeaderZIndex - 1,
+        }}
+      >
+        <AlertBox color="status-ok">
+          <Box direction="row" gap="small" align="center" justify="center">
+            <Info color="status-ok" size="20px" />
+            <Text size="12px">
+              <Trans
+                i18nKey="banner.domainMoved"
+                t={t}
+                components={{
+                  NewLink: <AnchorLink to="https://wallet.oasis.io/" label="https://wallet.oasis.io/" />,
+                }}
+                defaults="Oasis Wallet has a new home at <NewLink />."
+              />
+            </Text>
+          </Box>
+        </AlertBox>
+      </Box>
+    )
   }
 
   return (
