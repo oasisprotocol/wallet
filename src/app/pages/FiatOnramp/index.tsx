2913414d4765b4ad53f256fac2360edd147d6ceb
lukaw3d <lukajeran@oasislabs.com>
2023-04-27 03:37:45 +0200

    Require user to acknowledge before using a third-party service

--- src/app/pages/FiatOnramp/index.tsx
+++ src/app/pages/FiatOnramp/index.tsx
@@ -11,6 +11,8 @@ import { selectSelectedNetwork } from '../../state/network/selectors'
 import { selectAccountIsLoading } from '../../state/account/selectors'
 import { Button } from 'grommet/es6/components/Button'
 import { networkActions } from '../../state/network'
+import { CheckBox } from 'grommet/es6/components/CheckBox'
+import { useState } from 'react'
 
 function Layout(props: { children?: React.ReactNode }) {
   const { t } = useTranslation()
@@ -43,6 +45,7 @@ export function FiatOnramp() {
   const accountIsLoading = useSelector(selectAccountIsLoading)
   const isAddressInWallet = useSelector(selectIsAddressInWallet)
   const walletAddress = useSelector(selectAddress)
+  const [thirdPartyAcknowledged, setThirdPartyAcknowledged] = useState(false)
 
   if (selectedNetwork !== 'mainnet') {
     return (
@@ -91,29 +94,42 @@ export function FiatOnramp() {
         </Box>
       </AlertBox>
 
-      <iframe
-        height="825"
-        title="Transak On/Off Ramp Widget"
-        allow="camera;microphone;fullscreen;payment"
-        // TODO: maybe restrict top-navigation with sandbox=""
-        src={`${process.env.REACT_APP_TRANSAK_URL}/?${new URLSearchParams({
-          apiKey: process.env.REACT_APP_TRANSAK_PARTNER_ID,
-          productsAvailed: 'BUY',
-          cryptoCurrencyCode: 'ROSE',
-          walletAddress: walletAddress,
-          disableWalletAddressForm: 'true',
+      {!thirdPartyAcknowledged ? (
+        <Box style={{ height: 825 }}>
+          <CheckBox
+            label={t(
+              'fiatOnramp.thirdPartyAcknowledge',
+              'I understand that I’m using a third-party solution and Oasis does not carry any responsibility over the usage of this solution.',
+            )}
+            checked={thirdPartyAcknowledged}
+            onChange={event => setThirdPartyAcknowledged(event.target.checked)}
+          />
+        </Box>
+      ) : (
+        <iframe
+          height="825"
+          title="Transak On/Off Ramp Widget"
+          allow="camera;microphone;fullscreen;payment"
+          // TODO: maybe restrict top-navigation with sandbox=""
+          src={`${process.env.REACT_APP_TRANSAK_URL}/?${new URLSearchParams({
+            apiKey: process.env.REACT_APP_TRANSAK_PARTNER_ID,
+            productsAvailed: 'BUY',
+            cryptoCurrencyCode: 'ROSE',
+            walletAddress: walletAddress,
+            disableWalletAddressForm: 'true',
 
-          themeColor: '#18213c',
-        }).toString()}`}
-        style={{
-          display: 'block',
-          width: '100%',
-          maxHeight: '825px',
-          maxWidth: '500px',
-          borderRadius: '3px',
-          border: 'none',
-        }}
-      ></iframe>
+            themeColor: '#18213c',
+          }).toString()}`}
+          style={{
+            display: 'block',
+            width: '100%',
+            maxHeight: '825px',
+            maxWidth: '500px',
+            borderRadius: '3px',
+            border: 'none',
+          }}
+        ></iframe>
+      )}
     </Layout>
   )
 }
