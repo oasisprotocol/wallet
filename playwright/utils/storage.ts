9a4e6e8b92c943ee1e9432d87c4beeb07754db99
lukaw3d <lukajeran@oasislabs.com>
2023-05-12 20:57:36 +0200

    Slightly speedup playwright tests

--- playwright/utils/storage.ts
+++ playwright/utils/storage.ts
@@ -2,12 +2,14 @@ import type { Page } from '@playwright/test'
 import { privateKeyPersistedState } from './test-inputs'
 
 export async function clearPersistedStorage(page: Page) {
-  await page.goto('/')
+  // Move to the right domain, but don't needlessly load HTML and JS.
+  await page.goto('/app.webmanifest')
   await page.evaluate(() => window.localStorage.clear())
 }
 
 export async function addPersistedStorage(page: Page) {
-  await page.goto('/')
+  // Move to the right domain, but don't needlessly load HTML and JS.
+  await page.goto('/app.webmanifest')
   await page.evaluate(
     ([privateKeyPersistedState]) => {
       window.localStorage.setItem('oasis_wallet_persist_v1', privateKeyPersistedState)
