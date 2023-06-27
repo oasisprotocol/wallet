1ff127b420c75664d5bfab8494bbb933b71c8b38
lukaw3d <lukajeran@oasislabs.com>
2023-06-27 15:30:24 +0200

    Encourage decentralization by shuffling validators on every load

--- src/app/state/staking/saga.ts
+++ src/app/state/staking/saga.ts
@@ -13,6 +13,7 @@ import { getExplorerAPIs, getOasisNic } from '../network/saga'
 import { selectEpoch, selectSelectedNetwork } from '../network/selectors'
 import { selectValidators, selectValidatorsNetwork } from './selectors'
 import { CommissionBound, DebondingDelegation, Delegation, Validators } from './types'
+import shuffle from 'lodash/shuffle'
 
 function* loadDelegations(address: string) {
   const nic = yield* call(getOasisNic)
@@ -58,7 +59,7 @@ export function* refreshValidators() {
       stakingActions.updateValidators({
         timestamp: yield* call(now),
         network: network,
-        list: validators,
+        list: shuffle(validators),
       }),
     )
   } catch (errorApi: any) {
