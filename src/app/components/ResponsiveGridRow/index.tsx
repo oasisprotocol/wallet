ddff4e08015654f72e81102be46a9b48d72cb8bd
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-09-20 11:06:44 +0200

    Show contact name in tx confirmation dialog

--- src/app/components/ResponsiveGridRow/index.tsx
+++ src/app/components/ResponsiveGridRow/index.tsx
@@ -1,6 +1,9 @@
+import { useSelector } from 'react-redux'
 import { Box } from 'grommet/es6/components/Box'
 import { Text } from 'grommet/es6/components/Text'
 import React, { memo } from 'react'
+import { selectContact } from 'app/state/contacts/selectors'
+import { PrettyAddress } from '../PrettyAddress'
 
 interface ResponsiveGridRowProps {
   label: React.ReactNode
@@ -17,3 +20,21 @@ export const ResponsiveGridRow = memo(({ label, value }: ResponsiveGridRowProps)
     </>
   )
 })
+
+interface ResponsiveGridAddressRowProps {
+  label: string
+  value: string
+}
+
+export const ResponsiveGridAddressRow = ({ label, value }: ResponsiveGridAddressRowProps) => {
+  const contactAddress = useSelector(selectContact(value))
+
+  return (
+    <>
+      {contactAddress && (
+        <ResponsiveGridRow label={label} value={<Text weight="bold">{contactAddress.name}</Text>} />
+      )}
+      <ResponsiveGridRow label={contactAddress ? '' : label} value={<PrettyAddress address={value} />} />
+    </>
+  )
+}
