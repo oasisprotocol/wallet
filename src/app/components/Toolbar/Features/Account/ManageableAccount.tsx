6cfef2deb7333c472517591e62ec85b086fa929f
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-10-09 13:57:29 +0200

    Adjust My Accounts tab layout

--- src/app/components/Toolbar/Features/Account/ManageableAccount.tsx
+++ src/app/components/Toolbar/Features/Account/ManageableAccount.tsx
@@ -1,8 +1,9 @@
+import { useContext, useState } from 'react'
 import { Box } from 'grommet/es6/components/Box'
 import { Button } from 'grommet/es6/components/Button'
+import { ResponsiveContext } from 'grommet/es6/contexts/ResponsiveContext'
 import { Tab } from 'grommet/es6/components/Tab'
 import { Text } from 'grommet/es6/components/Text'
-import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Account } from './Account'
 import { Wallet } from '../../../../state/wallet/types'
@@ -10,6 +11,7 @@ import { Tabs } from 'grommet/es6/components/Tabs'
 import { DerivationFormatter } from './DerivationFormatter'
 import { uintToBase64, hex2uint } from '../../../../lib/helpers'
 import { AddressBox } from '../../../AddressBox'
+import { layerOverlayMinHeight } from '../layer'
 import { LayerContainer } from './../LayerContainer'
 
 export const ManageableAccount = ({
@@ -23,6 +25,7 @@ export const ManageableAccount = ({
 }) => {
   const { t } = useTranslation()
   const [layerVisibility, setLayerVisibility] = useState(false)
+  const isMobile = useContext(ResponsiveContext) === 'small'
 
   return (
     <>
@@ -41,28 +44,36 @@ export const ManageableAccount = ({
         <LayerContainer hideLayer={() => setLayerVisibility(false)}>
           <Tabs alignControls="start">
             <Tab title={t('toolbar.settings.myAccountsTab', 'My Accounts')}>
-              <Box margin={{ vertical: 'medium' }}>
-                <AddressBox address={wallet.address} border />
-                <Text size="small" margin={'small'}>
-                  <DerivationFormatter pathDisplay={wallet.pathDisplay} type={wallet.type} />
-                </Text>
-              </Box>
-              <Button
-                label={t('toolbar.settings.exportPrivateKey', 'Export Private Key')}
-                disabled={!wallet.privateKey}
-                onClick={() => {
-                  prompt(
-                    t('toolbar.settings.exportPrivateKey', 'Export Private Key'),
-                    uintToBase64(hex2uint(wallet.privateKey!)),
-                  )
-                }}
-              />
-              <Box direction="row" justify="between" pad={{ top: 'large' }}>
-                <Button
-                  secondary
-                  label={t('toolbar.settings.cancel', 'Cancel')}
-                  onClick={() => setLayerVisibility(false)}
-                />
+              <Box
+                flex="grow"
+                justify="between"
+                height={{ min: isMobile ? 'auto' : layerOverlayMinHeight }}
+                pad={{ vertical: 'medium' }}
+              >
+                <Box>
+                  <AddressBox address={wallet.address} border />
+                  <Text size="small" margin={'small'}>
+                    <DerivationFormatter pathDisplay={wallet.pathDisplay} type={wallet.type} />
+                  </Text>
+                  <Button
+                    alignSelf="start"
+                    label={t('toolbar.settings.exportPrivateKey', 'Export Private Key')}
+                    disabled={!wallet.privateKey}
+                    onClick={() => {
+                      prompt(
+                        t('toolbar.settings.exportPrivateKey', 'Export Private Key'),
+                        uintToBase64(hex2uint(wallet.privateKey!)),
+                      )
+                    }}
+                  />
+                </Box>
+                <Box direction="row" justify="between" pad={{ top: 'large' }}>
+                  <Button
+                    secondary
+                    label={t('toolbar.settings.cancel', 'Cancel')}
+                    onClick={() => setLayerVisibility(false)}
+                  />
+                </Box>
               </Box>
             </Tab>
           </Tabs>
