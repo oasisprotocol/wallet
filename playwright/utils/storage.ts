e07e958e5ed41205c86a1b8a2af4bde236b713e5
lukaw3d <lukajeran@oasislabs.com>
2023-11-29 04:21:27 +0100

    Fix addPersistedStorageV1 and  clearPersistedStorage in extension tests

--- playwright/utils/storage.ts
+++ playwright/utils/storage.ts
@@ -3,16 +3,21 @@ import { privateKeyPersistedState } from '../../src/utils/__fixtures__/test-inpu
 
 export async function clearPersistedStorage(
   page: Page,
-  url: '/app.webmanifest' | `chrome-extension://${string}`,
+  url: '/app.webmanifest' | `chrome-extension://${string}/manifest.json`,
 ) {
   // Move to the right domain, but don't needlessly load HTML and JS.
   await page.goto(url)
-  await page.evaluate(() => window.localStorage.clear())
+  await page.evaluate(() => {
+    window.localStorage.clear()
+
+    const chrome = (window as any).chrome
+    chrome?.extension?.getBackgroundPage?.().location.reload()
+  })
 }
 
 export async function addPersistedStorageV1(
   page: Page,
-  url: '/app.webmanifest' | `chrome-extension://${string}`,
+  url: '/app.webmanifest' | `chrome-extension://${string}/manifest.json`,
 ) {
   // Move to the right domain, but don't needlessly load HTML and JS.
   await page.goto(url)
