9e7b921ec2c8f3006eb67dcb408482669e9058bc
lukaw3d <lukajeran@oasislabs.com>
2024-01-08 18:54:04 +0100

    Allow ParaTimes with OasisMonitor backend (we use grpc to fetch balance)

--- src/app/pages/ParaTimesPage/useParaTimesNavigation.ts
+++ src/app/pages/ParaTimesPage/useParaTimesNavigation.ts
@@ -2,8 +2,6 @@ import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { paraTimesActions } from 'app/state/paratimes'
-import { backend } from 'vendors/backend'
-import { BackendAPIs } from 'config'
 import { WalletType } from 'app/state/wallet/types'
 import { selectType } from 'app/state/wallet/selectors'
 
@@ -24,7 +22,7 @@ export const useParaTimesNavigation = (): ParaTimesNavigationHook => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
   const walletType = useSelector(selectType)
-  const canAccessParaTimesRoute = backend() === BackendAPIs.OasisScan && walletType !== WalletType.UsbLedger
+  const canAccessParaTimesRoute = walletType !== WalletType.UsbLedger
   const getParaTimesRoutePath = (address: string) => `/account/${address}/paratimes`
   const navigateToDeposit = useCallback(() => dispatch(paraTimesActions.navigateToDeposit()), [dispatch])
   const navigateToWithdraw = useCallback(() => dispatch(paraTimesActions.navigateToWithdraw()), [dispatch])
