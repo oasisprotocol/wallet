87d7aee065a1143c2058c70078789b41d47c5088
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-04-16 16:32:14 +0200

    Further refinement of Oasis branding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.oasisprotocol.wallet',
-  appName: 'Oasis ROSE Wallet',
+  appName: 'ROSE Wallet',
   webDir: 'build',
   server: {
     androidScheme: 'https',
