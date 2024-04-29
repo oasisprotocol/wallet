585ad04eb7f8a44d6380f34e01390a963716b99c
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-04-29 06:24:32 +0200

    Fix USB Ledger accounts not enumerating after Ledger connected

--- src/app/pages/OpenWalletPage/webextension.tsx
+++ src/app/pages/OpenWalletPage/webextension.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { useHref } from 'react-router-dom'
+import { useHref, useNavigate } from 'react-router-dom'
 import { openLedgerAccessPopup } from 'utils/webextension'
 import { FromLedger } from './Features/FromLedger'
 
 export function FromLedgerWebExtension() {
   const href = useHref('/open-wallet/connect-device')
+  const navigate = useNavigate()
 
-  return <FromLedger webExtensionUSBLedgerAccess={() => openLedgerAccessPopup(href)} />
+  return (
+    <FromLedger
+      webExtensionUSBLedgerAccess={() => {
+        navigate('/open-wallet/ledger/usb')
+        openLedgerAccessPopup(href)
+      }}
+    />
+  )
 }
