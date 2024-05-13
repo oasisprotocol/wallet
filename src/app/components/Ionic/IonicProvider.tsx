44ac7d802ddca8c106ba9c93c46e49b41e8a7410
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-05-13 17:58:53 +0200

    Use local scope variable for backButton event handler

--- src/app/components/Ionic/IonicProvider.tsx
+++ src/app/components/Ionic/IonicProvider.tsx
@@ -1,11 +1,9 @@
 import { createContext, FC, PropsWithChildren, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Capacitor, type PluginListenerHandle } from '@capacitor/core'
+import { Capacitor } from '@capacitor/core'
 import { App } from '@capacitor/app'
 
 const IonicContext = createContext<undefined>(undefined)
-
-let backButtonListenerHandle: PluginListenerHandle | undefined
 
 const IonicContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const navigate = useNavigate()
@@ -15,7 +13,7 @@ const IonicContextProvider: FC<PropsWithChildren> = ({ children }) => {
      * The back button refers to the physical back button on an Android device and should not be confused
      * with either the browser back button or ion-back-button.
      */
-    backButtonListenerHandle = App.addListener('backButton', ({ canGoBack }) => {
+    const backButtonListenerHandle = App.addListener('backButton', ({ canGoBack }) => {
       if (!canGoBack) {
         App.exitApp()
       } else {
@@ -24,10 +22,7 @@ const IonicContextProvider: FC<PropsWithChildren> = ({ children }) => {
     })
 
     return () => {
-      if (backButtonListenerHandle) {
-        backButtonListenerHandle.remove()
-        backButtonListenerHandle = undefined
-      }
+      backButtonListenerHandle.remove()
     }
   }, [navigate])
 
