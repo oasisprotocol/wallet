80aed6ade6c53532ec65f4375a9c627f7181674a
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-05-22 18:12:05 +0200

    Update Android package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.oasisprotocol.wallet',
+  appId: 'org.oasisprotocol.wallet',
   appName: 'ROSE Wallet',
   webDir: 'build',
   server: {
