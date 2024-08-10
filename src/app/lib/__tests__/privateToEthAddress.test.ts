78c7d22e21f5aae92d620f418848a6730e2804bf
lukaw3d <lukajeran@oasislabs.com>
2024-08-10 21:00:34 +0200

    Fix privateToEthAddress in jest

--- src/app/lib/__tests__/privateToEthAddress.test.ts
+++ src/app/lib/__tests__/privateToEthAddress.test.ts
@@ -1,8 +1,6 @@
 import { privateToEthAddress } from '../eth-helpers'
 
-// TODO: enable when jest fixes `instanceof Uint8Array`. This throws "TypeError: Expected valid private key".
-// https://github.com/facebook/jest/issues/4422
-test.skip('privateToEthAddress', () => {
+test('privateToEthAddress', () => {
   expect(privateToEthAddress('414bba7f242e9054f9fc119fe32d322a7d9bbe0cb8c75173a6826cc8b1af1370')).toEqual(
     '0xFed5547859F9948d2C85F0516E3944377F88046f',
   )
