9d88f86e896b6e58a18e5ca26aa008be01cdcec4
lukaw3d <lukajeran@oasislabs.com>
2024-08-17 02:51:53 +0200

    Fix expanding table rows on non-plaintext cell
    Co-authored-by: Michal Zielenkiewicz <michal@oasisprotocol.org>

--- src/types/TypeSafeDataTable.tsx
+++ src/types/TypeSafeDataTable.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 // eslint-disable-next-line no-restricted-imports
 import DataTable, { IDataTableColumn, IDataTableProps } from 'react-data-table-component'
 
@@ -11,10 +12,47 @@ export interface ITypeSafeDataTableProps<T> extends Omit<IDataTableProps<T>, 'ke
 }
 
 /**
+ * Helper component for react-data-table-component custom cells
+ *
+ * react-data-table-component handles expandableRows correctly only for text nodes
+ * or it requires to apply data-tag="allowRowEvents" to the innermost element which is hard to control
+ *
+ * To prevent expanding add `onClick={e => e.stopPropagation()}`.
+ */
+function ExpandableCell(props: { children: ReactNode }) {
+  return (
+    <div
+      onClick={e => {
+        if (e.currentTarget.parentElement?.getAttribute('data-tag') !== 'allowRowEvents') {
+          throw new Error('Could not find table cell')
+        }
+        e.currentTarget.parentElement!.click()
+      }}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+/**
  * Overrides DataTable type to ensure:
  * - `keyField` on rows is not missing and is valid
  * - `id` on cols is not missing
+ *
+ * Fixes expanding rows when clicking non-plaintext cells.
  */
 export function TypeSafeDataTable<T>(props: ITypeSafeDataTableProps<T>): React.ReactElement {
-  return <DataTable {...props} />
+  return (
+    <DataTable
+      {...props}
+      columns={
+        props.columns.map(col => ({
+          ...col,
+          cell: (row, rowIndex, column, id) => {
+            return <ExpandableCell>{col.cell!(row, rowIndex, column, id)}</ExpandableCell>
+          },
+        })) as ITypeSafeDataTableColumn<T>[]
+      }
+    />
+  )
 }
