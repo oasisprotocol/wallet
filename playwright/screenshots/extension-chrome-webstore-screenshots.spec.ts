48d70aebdc7a5d6d1df04eb3aa3953b8523e21b7
lukaw3d <lukajeran@oasislabs.com>
2024-08-27 01:42:36 +0200

    Refactor screenshotting extension

--- playwright/screenshots/extension-chrome-webstore-screenshots.spec.ts
+++ playwright/screenshots/extension-chrome-webstore-screenshots.spec.ts
@@ -3,6 +3,7 @@ import { warnSlowApi } from '../utils/warnSlowApi'
 import { mockApi } from '../utils/mockApi'
 import { fillPrivateKeyWithoutPassword } from '../utils/fillPrivateKey'
 import { privateKey, privateKeyAddress } from '../../src/utils/__fixtures__/test-inputs'
+import { Page } from '@playwright/test'
 
 const screenshotCss = `
   * { scrollbar-width: none; }
@@ -16,8 +17,41 @@ const chromeWebStoreDimensions = {
 
 /** Scale for higher quality larger screenshots */
 const deviceScaleFactor = (chromeWebStoreDimensions.height - 20) / extensionViewport.height
+test.use({
+  deviceScaleFactor: deviceScaleFactor,
+  viewport: {
+    width: Math.round(chromeWebStoreDimensions.width / deviceScaleFactor),
+    height: Math.round(chromeWebStoreDimensions.height / deviceScaleFactor),
+  },
+})
 
-test.use({ deviceScaleFactor })
+async function setup(
+  page: Page,
+  extensionManifestURL: `chrome-extension://${string}/manifest.json`,
+  url: `chrome-extension://${string}`,
+) {
+  await page.goto(extensionManifestURL)
+  await page.setContent(`
+    <style>
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #e6e6e6;
+      }
+      iframe {
+        width: ${extensionViewport.width}px;
+        height: ${extensionViewport.height}px;
+        border: 5px solid #0500e2;
+      }
+    </style>
+    <body style="">
+      <iframe src="${url}"></iframe>
+    </body>
+  `)
+  return page.frameLocator('iframe')!
+}
 
 test.beforeEach(async ({ context }) => {
   await warnSlowApi(context)
@@ -26,31 +60,7 @@ test.beforeEach(async ({ context }) => {
 
 test.describe('make screenshots for Chrome Web Store', () => {
   test('start screen', async ({ page, extensionPopupURL, extensionManifestURL }) => {
-    await page.goto(extensionManifestURL)
-    await page.setViewportSize({
-      width: Math.round(chromeWebStoreDimensions.width / deviceScaleFactor),
-      height: Math.round(chromeWebStoreDimensions.height / deviceScaleFactor),
-    })
-
-    await page.setContent(`
-      <style>
-        body {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
-          background-color: #e6e6e6;
-        }
-        iframe {
-          width: ${extensionViewport.width}px;
-          height: ${extensionViewport.height}px;
-          border: 5px solid #0500e2;
-        }
-      </style>
-      <body style="">
-        <iframe src="${extensionPopupURL}/"></iframe>
-      </body>
-    `)
+    await setup(page, extensionManifestURL, `${extensionPopupURL}/`)
     await page.screenshot({
       path: './screenshots/extension-store-1.png',
       style: screenshotCss,
