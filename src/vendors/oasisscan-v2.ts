c96f4cf9a46aba806ecf244ece6162aaab345997
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-10-23 13:02:52 +0200

    Add Oasis Scan V2 validators list API

--- src/vendors/oasisscan-v2.ts
+++ src/vendors/oasisscan-v2.ts
@@ -1,4 +1,6 @@
-import { Configuration } from 'vendors/oasisscan-v2/index'
+import { Validator } from 'app/state/staking/types'
+import { parseRoseStringToBaseUnitString } from 'app/lib/helpers'
+import { Configuration, ValidatorApi, ValidatorInfo } from 'vendors/oasisscan-v2/index'
 
 import { throwAPIErrors } from './helpers'
 
@@ -8,5 +10,41 @@ export function getOasisscanV2APIs(url: string | 'https://api.oasisscan.com/v2/m
     ...throwAPIErrors,
   })
 
-  return {}
+  const validatorApi = new ValidatorApi(explorerConfig)
+
+  async function getAllValidators(): Promise<Validator[]> {
+    const validators = await validatorApi.validatorListHandler({ orderBy: 'escrow', sort: 'desc' })
+    if (!validators) throw new Error('Wrong response code')
+
+    return parseValidatorsList(validators.list)
+  }
+
+  async function getTransactionsList(params: { accountId: string; limit: number }) {
+    return []
+  }
+
+  return {
+    getAllValidators,
+    getTransactionsList,
+  }
+}
+
+function parseValidatorsList(validators: ValidatorInfo[]): Validator[] {
+  return validators.map(v => {
+    const parsed: Validator = {
+      address: v.entityAddress,
+      name: v.name ?? undefined,
+      escrow: parseRoseStringToBaseUnitString(v.escrow),
+      current_rate: v.commission,
+      status: v.status ? 'active' : 'inactive',
+      media: {
+        email_address: v.email ?? undefined,
+        logotype: v.icon ?? undefined,
+        twitter_acc: v.twitter ?? undefined,
+        website_link: v.website ?? undefined,
+      },
+      rank: v.rank,
+    }
+    return parsed
+  })
 }
