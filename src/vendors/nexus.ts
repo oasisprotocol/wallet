a4596114b19bc8f8b386f03e362a4e175c0529aa
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-10-25 15:53:12 +0200

    Add Nexus delegations API

--- src/vendors/nexus.ts
+++ src/vendors/nexus.ts
@@ -1,6 +1,13 @@
-import { Configuration, DefaultApi as NexusApi, Validator as NexusValidator } from 'vendors/nexus/index'
+import * as oasis from '@oasisprotocol/client'
+import {
+  Configuration,
+  Delegation as NexusDelegation,
+  DebondingDelegation as NexusDebondingDelegation,
+  DefaultApi as NexusApi,
+  Validator as NexusValidator,
+} from 'vendors/nexus/index'
+import { DebondingDelegation, Delegation, Validator } from 'app/state/staking/types'
 import { StringifiedBigInt } from 'types/StringifiedBigInt'
-import { Validator } from 'app/state/staking/types'
 import { throwAPIErrors } from './helpers'
 
 export function getNexusAPIs(url: string | 'https://nexus.oasis.io/v1/') {
@@ -22,9 +29,29 @@ export function getNexusAPIs(url: string | 'https://nexus.oasis.io/v1/') {
     return []
   }
 
+  async function getDelegations(params: { accountId: string; nic: oasis.client.NodeInternal }): Promise<{
+    delegations: Delegation[]
+    debonding: DebondingDelegation[]
+  }> {
+    const delegations = await api.consensusAccountsAddressDelegationsGet({
+      address: params.accountId,
+    })
+    const debonding = await api.consensusAccountsAddressDebondingDelegationsGet({
+      address: params.accountId,
+    })
+    if (!delegations) throw new Error('Wrong response code')
+    if (!debonding) throw new Error('Wrong response code')
+
+    return {
+      delegations: parseDelegations(delegations.delegations),
+      debonding: parseDebonding(debonding.debonding_delegations),
+    }
+  }
+
   return {
     getAllValidators,
     getTransactionsList,
+    getDelegations,
   }
 }
 
@@ -43,6 +70,29 @@ function parseValidatorsList(validators: NexusValidator[]): Validator[] {
         website_link: v.media?.url ?? undefined,
       },
       rank: v.rank,
+    }
+    return parsed
+  })
+}
+
+export function parseDelegations(delegations: NexusDelegation[]): Delegation[] {
+  return delegations.map(delegation => {
+    const parsed: Delegation = {
+      amount: delegation.amount,
+      shares: delegation.shares,
+      validatorAddress: delegation.validator,
+    }
+    return parsed
+  })
+}
+
+export function parseDebonding(debonding: NexusDebondingDelegation[]): DebondingDelegation[] {
+  return debonding.map(debonding => {
+    const parsed: DebondingDelegation = {
+      amount: debonding.shares,
+      shares: debonding.shares,
+      validatorAddress: debonding.validator,
+      epoch: debonding.debond_end,
     }
     return parsed
   })
