736928b8e2e45781969768fac5255f1decc7a3c9
lukaw3d <lukajeran@oasislabs.com>
2024-11-01 06:33:29 +0100

    iteration 1: Open ext as a single tab

--- extension/src/popup/popup.tsx
+++ extension/src/popup/popup.tsx
@@ -6,25 +6,49 @@ import { configureAppStore } from 'store/configureStore'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 import { ThemeProvider } from 'styles/theme/ThemeProvider'
+import browser from 'webextension-polyfill'
 
 import 'locales/i18n'
 import 'sanitize.css/sanitize.css'
 import 'styles/main.css'
 import { routes } from './routes'
 
-const container = document.getElementById('root') as HTMLElement
-const root = createRoot(container!)
-const store = configureAppStore()
-const router = createHashRouter(routes)
-
-root.render(
-  <Provider store={store}>
-    <ThemeProvider>
-      <HelmetProvider>
-        <React.StrictMode>
-          <RouterProvider router={router} />
-        </React.StrictMode>
-      </HelmetProvider>
-    </ThemeProvider>
-  </Provider>,
-)
+// Open as a single tab:
+// - single: so no need to sync between tabs
+// - tab: so it doesn't constantly close like popup, and lose unlocked state
+;(async () => {
+  const singleTabId = (await browser.runtime.getContexts({ contextTypes: ['TAB'] }))[0]?.tabId
+  const isTabOrPopup = (await browser.tabs.getCurrent()) ? 'tab' : 'popup'
+  if (isTabOrPopup === 'popup') {
+    if (singleTabId) {
+      console.log('Focus existing tab and close popup.')
+      await browser.tabs.update(singleTabId, { active: true })
+      window.close()
+    } else {
+      console.log('Open new tab and close popup.')
+      await browser.tabs.create({ url: window.location.href })
+      window.close()
+    }
+  } else if (browser.extension.getViews({ type: 'tab' }).length > 1) {
+    console.log('This is a second tab. Close and focus first one.')
+    await browser.tabs.update(singleTabId, { active: true })
+    window.close()
+  } else {
+    console.log('This is the single tab.')
+    const container = document.getElementById('root') as HTMLElement
+    const root = createRoot(container!)
+    const store = configureAppStore()
+    const router = createHashRouter(routes)
+    root.render(
+      <Provider store={store}>
+        <ThemeProvider>
+          <HelmetProvider>
+            <React.StrictMode>
+              <RouterProvider router={router} />
+            </React.StrictMode>
+          </HelmetProvider>
+        </ThemeProvider>
+      </Provider>,
+    )
+  }
+})()
