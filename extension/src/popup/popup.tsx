9685f6a405465661126e80e756e3ae66b43abf95
lukaw3d <lukajeran@oasislabs.com>
2024-11-01 22:52:31 +0100

    iteration 2: Open ext as a single popup

--- extension/src/popup/popup.tsx
+++ extension/src/popup/popup.tsx
@@ -13,28 +13,46 @@ import 'sanitize.css/sanitize.css'
 import 'styles/main.css'
 import { routes } from './routes'
 
-// Open as a single tab:
+// Open as a single persistent popup:
 // - single: so no need to sync between tabs
-// - tab: so it doesn't constantly close like popup, and lose unlocked state
+// - persistent popup: so it doesn't constantly close like popup, and lose unlocked state
 ;(async () => {
-  const singleTabId = (await browser.runtime.getContexts({ contextTypes: ['TAB'] }))[0]?.tabId
+  // Unexpected: persistent popup is classified as 'TAB' in contexts API
+  const singlePersistentPopupId = (await browser.runtime.getContexts({ contextTypes: ['TAB'] }))[0]?.windowId
   const isTabOrPopup = (await browser.tabs.getCurrent()) ? 'tab' : 'popup'
   if (isTabOrPopup === 'popup') {
-    if (singleTabId) {
-      console.log('Focus existing tab and close popup.')
-      await browser.tabs.update(singleTabId, { active: true })
+    if (singlePersistentPopupId) {
+      console.log('Focus existing persistent popup.')
+      await browser.windows.update(singlePersistentPopupId, { focused: true })
       window.close()
     } else {
-      console.log('Open new tab and close popup.')
-      await browser.tabs.create({ url: window.location.href })
+      console.log('Open new persistent popup.')
+      // Add slight delay so `window.screenLeft` returns popup's position, not extension's icon position
+      await new Promise(r => setTimeout(r, 100))
+      const position = {
+        left: window.screenLeft,
+        top: window.screenTop + 30,
+        width: 400,
+        height: 600,
+      }
+      const newPopup = await browser.windows.create({
+        url: window.location.href,
+        type: 'popup',
+        left: Math.max(Math.min(position.left, window.screen.width - position.width), 0),
+        top: Math.max(Math.min(position.top, window.screen.height - position.height), 0),
+        width: position.width,
+        height: position.height,
+        focused: true,
+      })
+      await browser.windows.update(newPopup.id!, { focused: true }) // Focus again. Helps in rare cases like when screensharing.
       window.close()
     }
   } else if (browser.extension.getViews({ type: 'tab' }).length > 1) {
     console.log('This is a second tab. Close and focus first one.')
-    await browser.tabs.update(singleTabId, { active: true })
+    await browser.windows.update(singlePersistentPopupId, { focused: true })
     window.close()
   } else {
-    console.log('This is the single tab.')
+    console.log('This is the single persistent popup.')
     await browser.action.setBadgeText({ text: '🗔' })
     window.addEventListener('beforeunload', () => {
       browser.action.setBadgeText({ text: null })
