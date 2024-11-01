1a0a61f12fe9907f85f0f745e3bb25e90bec6d8c
lukaw3d <lukajeran@oasislabs.com>
2024-11-01 06:30:38 +0100

    Remove bg page and migrate to manifest v3 (each popup/tab has own state)

--- extension/src/popup/popup.tsx
+++ extension/src/popup/popup.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { HelmetProvider } from 'react-helmet-async'
-import { Store } from 'webext-redux'
+import { configureAppStore } from 'store/configureStore'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 import { ThemeProvider } from 'styles/theme/ThemeProvider'
@@ -14,21 +14,17 @@ import { routes } from './routes'
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container!)
-const store = new Store()
+const store = configureAppStore()
 const router = createHashRouter(routes)
 
-store.ready().then(() => {
-  root.render(
-    <Provider store={store}>
-      <ThemeProvider>
-        <HelmetProvider>
-          <React.StrictMode>
-            <RouterProvider router={router} />
-          </React.StrictMode>
-        </HelmetProvider>
-      </ThemeProvider>
-    </Provider>,
-  )
-})
-
-console.log('popup')
+root.render(
+  <Provider store={store}>
+    <ThemeProvider>
+      <HelmetProvider>
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+      </HelmetProvider>
+    </ThemeProvider>
+  </Provider>,
+)
