7e6b632d9d0bc6099bc28ebab5aeecc9c1766175
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-11-04 15:00:17 +0100

    Add Nexus validators API

--- src/vendors/nexus.ts
+++ src/vendors/nexus.ts
@@ -1,12 +1,49 @@
-import { Configuration } from 'vendors/nexus/index'
-
+import { Configuration, DefaultApi as NexusApi, Validator as NexusValidator } from 'vendors/nexus/index'
+import { StringifiedBigInt } from 'types/StringifiedBigInt'
+import { Validator } from 'app/state/staking/types'
 import { throwAPIErrors } from './helpers'
 
-export function getNexusAPIs(url: string | 'https://nexus.oasis.io/v1/consensus/') {
+export function getNexusAPIs(url: string | 'https://nexus.oasis.io/v1/') {
   const explorerConfig = new Configuration({
     basePath: url,
     ...throwAPIErrors,
   })
 
-  return {}
+  const api = new NexusApi(explorerConfig)
+
+  async function getAllValidators(): Promise<Validator[]> {
+    const validatorsResponse = await api.consensusValidatorsGet({})
+    if (!validatorsResponse) throw new Error('Wrong response code')
+
+    return parseValidatorsList(validatorsResponse.validators)
+  }
+
+  async function getTransactionsList(params: { accountId: string; limit: number }) {
+    return []
+  }
+
+  return {
+    getAllValidators,
+    getTransactionsList,
+  }
+}
+
+function parseValidatorsList(validators: NexusValidator[]): Validator[] {
+  return validators.map(v => {
+    const parsed: Validator = {
+      address: v.entity_address,
+      name: v.media?.name ?? undefined,
+      escrow: v.escrow.active_balance as StringifiedBigInt,
+      current_rate: v.current_rate / 100000,
+      status: v.active ? 'active' : 'inactive',
+      media: {
+        email_address: v.media?.email ?? undefined,
+        logotype: v.media?.logoUrl ?? undefined,
+        twitter_acc: v.media?.twitter ?? undefined,
+        website_link: v.media?.url ?? undefined,
+      },
+      rank: v.rank,
+    }
+    return parsed
+  })
 }
