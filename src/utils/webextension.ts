29fa1f11d050f6268758f4d85d23d72848d376a3
lukaw3d <lukajeran@oasislabs.com>
2024-11-06 04:32:51 +0100

    Focus ledger access popup twice

--- src/utils/webextension.ts
+++ src/utils/webextension.ts
@@ -13,12 +13,14 @@ const openPopup = async ({ path, height, width, type }: Props) => {
   if (existingPopupWindow) {
     existingPopupWindow.close()
   }
-  browser.windows.create({
+  const popup = await browser.windows.create({
     url: path,
     type: type ?? 'popup',
     width: width,
     height: height,
+    focused: true,
   })
+  await browser.windows.update(popup.id!, { focused: true }) // Focus again. Helps in rare cases like when screensharing.
 }
 
 export const openLedgerAccessPopup = (path: string) => {
