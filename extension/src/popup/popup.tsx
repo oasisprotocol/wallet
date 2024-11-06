7671cb9ed75c015fb296ddd8f04d7f754c6906b1
lukaw3d <lukajeran@oasislabs.com>
2024-11-06 02:41:48 +0100

    focus ExtensionRequestLedgerPermissionPopup if it exists when user clicks wallet again

--- extension/src/popup/popup.tsx
+++ extension/src/popup/popup.tsx
@@ -16,8 +16,18 @@ import { routes } from './routes'
 // Open as a single popup:
 // - single: so no need to sync between tabs
 if (browser.extension.getViews({ type: 'tab' }).length > 0) {
-  console.log('This is a tab. Close.')
-  window.close()
+  // Either this is a tab, or something else is.
+  // If this is a tab, just close.
+  // If something else is a tab, it must be ExtensionRequestLedgerPermissionPopup. Focus that and close self.
+  ;(async () => {
+    // Unexpected: persistent popup is classified as 'TAB' in contexts API
+    const tabsAndPersistentPopups = await browser.runtime.getContexts({ contextTypes: ['TAB'] })
+    for (const c of tabsAndPersistentPopups) {
+      await browser.windows.update(c.windowId, { focused: true })
+      await browser.tabs.update(c.tabId, { active: true })
+    }
+    window.close()
+  })()
 } else {
   const container = document.getElementById('root') as HTMLElement
   const root = createRoot(container!)
