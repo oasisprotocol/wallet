dee78c2f088878c2e6c1251f5834940173b8f9ac
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-11-08 16:38:44 +0100

    Bump webext-redux

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,6 +1,7 @@
-import { wrapStore } from 'webext-redux'
+import { createWrapStore } from 'webext-redux'
 import { configureAppStore } from 'store/configureStore'
 
+const wrapStore = createWrapStore()
 const store = configureAppStore()
 
 wrapStore(store)
