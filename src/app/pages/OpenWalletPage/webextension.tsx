ccf932e27c11c74f5eb7de911c34fdf06f4f38d3
lukaw3d <lukajeran@oasislabs.com>
2024-11-12 17:28:30 +0700

    Refactor how extension requests ledger access (don't rely on state sync)
    Before:
    - redux was shared between persistent popup (can request USB permissions) and
      default popup
    - persistent popup listed ledger accounts into store
    - when reopening default popup listed accounts were kept
    - when reopening default popup after clearing listed accounts: all steps repeat
    
    After:
    - persistent popup requests permissions
    - this gives permissions to default popup too
    - when reopening default popup: use new permissions to list accounts from ledger

--- src/app/pages/OpenWalletPage/webextension.tsx
+++ src/app/pages/OpenWalletPage/webextension.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { useHref, useNavigate } from 'react-router-dom'
+import { useHref } from 'react-router-dom'
 import { openLedgerAccessPopup } from 'utils/webextension'
 import { FromLedger } from './Features/FromLedger'
 
 export function FromLedgerWebExtension() {
   const href = useHref('/open-wallet/connect-device')
-  const navigate = useNavigate()
 
   return (
     <FromLedger
       openLedgerAccessPopup={() => {
-        navigate('/open-wallet/ledger/usb')
         openLedgerAccessPopup(href)
       }}
     />
