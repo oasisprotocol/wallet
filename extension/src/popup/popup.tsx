a534682b03624555afd0187b0ee8540f36b475cc
lukaw3d <lukajeran@oasislabs.com>
2025-01-25 15:00:55 +0700

    Allow only one extension popup, so no need to sync state

--- extension/src/popup/popup.tsx
+++ extension/src/popup/popup.tsx
@@ -6,25 +6,41 @@ import { configureAppStore } from 'store/configureStore'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 import { ThemeProvider } from 'styles/theme/ThemeProvider'
+import browser from 'webextension-polyfill'
 
 import 'locales/i18n'
 import 'sanitize.css/sanitize.css'
 import 'styles/main.css'
 import { routes } from './routes'
 
-const container = document.getElementById('root') as HTMLElement
-const root = createRoot(container!)
-const store = configureAppStore()
-const router = createHashRouter(routes)
-
-root.render(
-  <Provider store={store}>
-    <ThemeProvider>
-      <HelmetProvider>
-        <React.StrictMode>
-          <RouterProvider router={router} />
-        </React.StrictMode>
-      </HelmetProvider>
-    </ThemeProvider>
-  </Provider>,
-)
+// Only allow one popup with redux, so no need to sync state.
+if (browser.extension.getViews({ type: 'tab' }).length > 0) {
+  // Either this is a tab, or something else is.
+  // If this is a tab, just close.
+  // If something else is a tab, it must be ExtLedgerAccessPopup. Focus that and close self.
+  ;(async () => {
+    // Unexpected: persistent popup is classified as 'TAB' in contexts API
+    const tabsAndPersistentPopups = await browser.runtime.getContexts({ contextTypes: ['TAB'] })
+    for (const c of tabsAndPersistentPopups) {
+      await browser.windows.update(c.windowId, { focused: true })
+      await browser.tabs.update(c.tabId, { active: true })
+    }
+    window.close()
+  })()
+} else {
+  const container = document.getElementById('root') as HTMLElement
+  const root = createRoot(container!)
+  const store = configureAppStore()
+  const router = createHashRouter(routes)
+  root.render(
+    <Provider store={store}>
+      <ThemeProvider>
+        <HelmetProvider>
+          <React.StrictMode>
+            <RouterProvider router={router} />
+          </React.StrictMode>
+        </HelmetProvider>
+      </ThemeProvider>
+    </Provider>,
+  )
+}
