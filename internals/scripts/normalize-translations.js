981fe109da1efede8c2df82d2280de334cdf44e6
lukaw3d <lukajeran@oasislabs.com>
2025-03-15 04:15:55 +0700

    Prune translations

--- internals/scripts/normalize-translations.js
+++ internals/scripts/normalize-translations.js
@@ -4,22 +4,38 @@
  * empty strings and then empty groups. react-i18next will automatically
  * use the English version of the string, if the translation does not exist.
  *
+ * Additionally, extract-messages prunes english lang when we remove some
+ * translation keys, but it doesn't prune other translations.
+ *
  * Usage: `node ./normalize-translations.js ./path/*.json`
  */
 
 const paths = process.argv.slice(2)
+const englishPath = paths.find(path => path.includes('/en/translation.json'))
+if (!englishPath) throw new Error('Can not find english translation')
+const english = JSON.parse(require('fs').readFileSync(englishPath, 'utf-8'))
+
 paths.forEach(path => {
-  const translationStr = require('fs').readFileSync(path, 'utf-8')
+  const translation = JSON.parse(require('fs').readFileSync(path, 'utf-8'))
 
-  const cleanedUpTranslation = JSON.stringify(
-    JSON.parse(translationStr, (k, v) => {
-      if (v === '') return // Remove untranslated "" fields
-      if (Object.getPrototypeOf(v) === Object.prototype && Object.keys(v).length === 0) return // Remove empty groups {}
-      return v
-    }),
-    null,
-    2,
-  )
+  pruneTranslation(translation, english)
 
-  require('fs').writeFileSync(path, `${cleanedUpTranslation}\n`, 'utf-8')
+  const cleanedUpTranslation = JSON.parse(JSON.stringify(translation), (k, v) => {
+    if (v === '') return // Remove untranslated "" fields
+    if (Object.getPrototypeOf(v) === Object.prototype && Object.keys(v).length === 0) return // Remove empty groups {}
+    return v
+  })
+
+  require('fs').writeFileSync(path, `${JSON.stringify(cleanedUpTranslation, null, 2)}\n`, 'utf-8')
 })
+
+/** Remove keys if they don't appear in english translation */
+function pruneTranslation(prunableTranslationObj, englishObj) {
+  for (const key in prunableTranslationObj) {
+    if (!(key in englishObj)) {
+      delete prunableTranslationObj[key]
+    } else if (typeof prunableTranslationObj[key] === 'object') {
+      pruneTranslation(prunableTranslationObj[key], englishObj[key])
+    }
+  }
+}
