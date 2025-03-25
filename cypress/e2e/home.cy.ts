7ab46f9fc6417679667333b997fedc65dca28d78
lukaw3d <lukajeran@oasislabs.com>
2025-03-25 14:03:06 +0100

    Remove some unneeded cypress tests already covered by playwright

--- cypress/e2e/home.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('The homepage should load', () => {
-  it('should have options to open the wallet', () => {
-    cy.visit('/')
-    cy.findByRole('link', { name: /Open wallet/i })
-    cy.findByRole('link', { name: /Create wallet/i })
-  })
-})
--- cypress/e2e/open-wallet.cy.ts
+++ cypress/e2e/open-wallet.cy.ts
@@ -5,74 +5,7 @@ describe('Open wallet', () => {
     cy.intercept('/data/accounts/*', { body: JSON.stringify(account) })
   })
 
-  describe('Method selection', () => {
-    it('Should be able to open from Mnemonic', () => {
-      cy.visit('/open-wallet')
-      cy.findByRole('link', { name: /Mnemonic/ }).click()
-      cy.url().should('include', 'mnemonic')
-    })
-
-    it('Should be able to open from private key', () => {
-      cy.visit('/open-wallet')
-      cy.findByRole('link', { name: /Private key/ }).click()
-      cy.url().should('include', 'private-key')
-    })
-  })
-
-  describe('Mnemonic', () => {
-    const mnemonic = 'planet believe session regular rib kiss police deposit prison hundred apart tongue'
-    beforeEach(() => {
-      cy.visit('/open-wallet/mnemonic')
-    })
-
-    it('Should reject invalid mnemonics', () => {
-      cy.findByPlaceholderText('Enter your mnemonic here').type('this is an invalid mnemonic')
-      cy.findByRole('button', { name: /Import my wallet/ }).click()
-      cy.findByText(/Invalid mnemonic/).should('be.visible')
-    })
-
-    it('Should accept valid mnemonic', () => {
-      cy.findByPlaceholderText('Enter your mnemonic here').type(mnemonic, { delay: 1 })
-      cy.findByRole('button', { name: /Import my wallet/ }).click()
-      cy.findByText(/Invalid mnemonic/).should('not.exist')
-      cy.findByRole('checkbox', { name: /Create a profile/ }).uncheck({ force: true })
-      cy.findByRole('button', { name: /Open/ }).click()
-      cy.url().should('include', 'oasis1qqca0gplrfn63ljg9c833te7em36lkz0cv8djffh')
-    })
-
-    it('Should open multiple accounts from mnemonic', () => {
-      cy.findByPlaceholderText('Enter your mnemonic here').type(mnemonic, { delay: 1 })
-      cy.findByRole('button', { name: /Import my wallet/ }).click()
-      cy.findByRole('checkbox', { name: /Create a profile/ }).uncheck({ force: true })
-      cy.findAllByTestId('account-choice').should('have.length', 4)
-      cy.findAllByRole('checkbox', { name: /oasis1/, checked: false })
-        .should('have.length', 3)
-        .click({ force: true, multiple: true })
-
-      cy.findByRole('button', { name: /Open/ }).click()
-      cy.url().should('include', 'oasis1qqca0gplrfn63ljg9c833te7em36lkz0cv8djffh')
-
-      cy.log('Should allow importing and re-selecting the same accounts')
-      cy.findByRole('link', { name: /Home/ }).click()
-      cy.findByRole('link', { name: /Open wallet/ }).click()
-      cy.findByRole('link', { name: /Mnemonic/ }).click()
-      cy.url().should('include', '/open-wallet/mnemonic')
-
-      cy.findByPlaceholderText('Enter your mnemonic here').type(mnemonic, { delay: 1 })
-      cy.findByRole('button', { name: /Import my wallet/ }).click()
-      cy.findByRole('checkbox', { name: /Create a profile/ }).uncheck({ force: true })
-      cy.findAllByTestId('account-choice').should('have.length', 4)
-      cy.findAllByRole('checkbox', { name: /oasis1/, checked: false })
-        .should('have.length', 3)
-        .click({ force: true, multiple: true })
-
-      cy.findByRole('button', { name: /Open/ }).click()
-      cy.url().should('include', 'oasis1qqca0gplrfn63ljg9c833te7em36lkz0cv8djffh')
-    })
-  })
-
   describe('Private Key', () => {
-    const privateKey = 'X0jlpvskP1q8E6rHxWRJr7yTvpCuOPEKBGW8gtuVTxfnViTI0s2fBizgMxNzo75Q7w7MxdJXtOLeqDoFUGxxMg=='
     beforeEach(() => {
       cy.visit('/open-wallet/private-key')
       cy.findByRole('checkbox', { name: /Create a profile/ }).uncheck({ force: true })
@@ -92,47 +25,5 @@ describe('Open wallet', () => {
       cy.findByRole('button', { name: /Import my wallet/ }).click()
       cy.findByText(/Invalid private key/).should('be.visible')
     })
-
-    it('Should accept valid base64 pkey', () => {
-      cy.findByPlaceholderText('Enter your private key here').type(privateKey, { delay: 1 })
-      cy.findByRole('button', { name: /Import my wallet/ }).click()
-      cy.findByText(/Invalid private key/).should('not.exist')
-      cy.url().should('include', '/account/oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk')
-
-      cy.log('Should allow importing and re-selecting the same account')
-      cy.findByRole('link', { name: /Home/ }).click()
-      cy.findByRole('link', { name: /Open wallet/ }).click()
-      cy.findByRole('link', { name: /Private key/ }).click()
-      cy.url().should('include', '/open-wallet/private-key')
-      cy.findByRole('checkbox', { name: /Create a profile/ }).uncheck({ force: true })
-      cy.findByPlaceholderText('Enter your private key here').type(privateKey, { delay: 1 })
-      cy.findByRole('button', { name: /Import my wallet/ }).click()
-      cy.url().should('include', '/account/oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk')
-    })
   })
-
-  // it('should not be able to submit without confirmation', () => {
-  //   cy.findByRole('button', { name: /Import my wallet/ }).should('be.disabled')
-  // });
-
-  // it('should be able to submit with confirmation', () => {
-  //   cy.findByLabelText(/saved/).click({ force: true })
-  //   cy.findByRole('button', { name: /Import my wallet/ }).should('be.enabled')
-  // });
-
-  // it('Should open wallet', () => {
-  //   cy.findByLabelText(/saved/).click({ force: true })
-  //   cy.findByRole('button', { name: /Import my wallet/ }).should('be.enabled').click()
-  //   cy.url().should('include', '/wallet')
-  // })
-
-  // it('Should be able to close the wallet once opened', () => {
-  //   cy.findByLabelText(/saved/).click({ force: true })
-  //   cy.findByRole('button', { name: /Import my wallet/i }).click()
-  //   cy.url().should('include', '/wallet')
-  //   cy.get('button[aria-label="Close Wallet"]').click()
-
-  //   // Back to homepage
-  //   cy.findByRole('link', { name: /Create wallet/i }).should('be.visible')
-  // })
 })
