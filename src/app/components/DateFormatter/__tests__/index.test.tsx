c7548e79e4a9491e31981e175308408043ffa921
lukaw3d <luka@oasisprotocol.org>
2025-10-09 07:06:12 +0700

    Remove unused DateFormatter component

--- src/app/components/DateFormatter/__tests__/index.test.tsx
+++ src/app/components/DateFormatter/__tests__/index.test.tsx
@@ -1,6 +1,3 @@
-import { render, screen } from '@testing-library/react'
-
-import { DateFormatter } from '..'
 import { intlDateTimeFormat } from '../intlDateTimeFormat'
 import { buildDatetime } from '../../../../../internals/getBuildData'
 
@@ -11,12 +8,5 @@ describe('intlDateTimeFormat', () => {
   })
   it('build time should be in the same format as a tested timestamp', () => {
     expect(Number(buildDatetime)).toBeCloseTo(1656932251000, -12) // Less than 16 years difference
-  })
-})
-
-describe('<DateFormatter  />', () => {
-  it('should render component with formatted date', () => {
-    render(<DateFormatter date={1656932251000} />)
-    expect(screen.getByText('7/4/2022, 10:57:31 AM')).toBeInTheDocument()
   })
 })
