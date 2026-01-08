a290179e2162816f37619573eaf42997b221c4e4
Michal Zielenkiewicz <michal@oasisprotocol.org>
2026-01-08 09:12:20 +0100

    Prepare redux state for controlling mobile update gate

--- src/app/state/settings/slice/index.ts
+++ src/app/state/settings/slice/index.ts
@@ -1,11 +1,17 @@
 import { PayloadAction } from '@reduxjs/toolkit'
 import { createSlice } from 'utils/@reduxjs/toolkit'
 
-import { getScreenPrivacyFromStorage, saveScreenPrivacy } from '../utils'
-import { ScreenPrivacyType, SettingsState } from './types'
+import {
+  getScreenPrivacyFromStorage,
+  getUpdateGateCheckFromStorage,
+  saveScreenPrivacy,
+  saveUpdateGateCheck,
+} from '../utils'
+import { UpdateGateCheckType, ScreenPrivacyType, SettingsState } from './types'
 
 export const getInitialState = (): SettingsState => ({
   screenPrivacy: getScreenPrivacyFromStorage() || 'on',
+  updateGateCheck: getUpdateGateCheckFromStorage() || 'on',
 })
 
 export const settingsSlice = createSlice({
@@ -15,6 +21,10 @@ export const settingsSlice = createSlice({
     changeScreenPrivacy(state, action: PayloadAction<ScreenPrivacyType>) {
       saveScreenPrivacy(action.payload)
       state.screenPrivacy = action.payload
+    },
+    changeUpdateGateCheck(state, action: PayloadAction<UpdateGateCheckType>) {
+      saveUpdateGateCheck(action.payload)
+      state.updateGateCheck = action.payload
     },
   },
 })
--- src/app/state/settings/slice/selectors.ts
+++ src/app/state/settings/slice/selectors.ts
@@ -5,3 +5,4 @@ import { getInitialState } from '.'
 
 const selectSlice = (state: RootState) => state.settings || getInitialState()
 export const selectScreenPrivacy = createSelector([selectSlice], settings => settings.screenPrivacy)
+export const selectUpdateGateCheck = createSelector([selectSlice], settings => settings.updateGateCheck)
